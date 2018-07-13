d90f11a26d906760e66cf8cce706a9ff415b227b
Chih-Hung Hsieh <chh@google.com>
2018-07-13 13:26:13 -0700

    Fix unnecessary copy initialization warnings
    Bug: 30413862
    Test: build with WITH_TIDY=1
    Change-Id: I74e18cb3a52e7805c108963322eba69e51f2f92d

--- common/CpuExecutor.cpp
+++ common/CpuExecutor.cpp
@@ -35,7 +35,7 @@ RunTimePoolInfo::RunTimePoolInfo(const hidl_memory& hidlMemory, bool* fail) {
     sp<IMemory> memory;
     uint8_t* buffer = nullptr;
 
-    auto memType = hidlMemory.name();
+    const auto& memType = hidlMemory.name();
     if (memType == "ashmem") {
         memory = mapMemory(hidlMemory);
         if (memory == nullptr) {
