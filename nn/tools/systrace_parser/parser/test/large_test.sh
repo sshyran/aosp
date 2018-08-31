b46d9cb242cf75af0c55b689820f9ad1f3a7dd1f
Mika Raento <mikie@google.com>
2018-08-31 09:54:32 +0100

    Parse incomplete systraces correctly
    Improves parser to handle:
    - systraces that are cut in the middle of running NNAPI
    - systraces from O-MR1 that don't have tracing in NNAPI runtime
    
    Test: tools/parser/run_test.sh
    Bug: 112573283
    Change-Id: Icfacc785ab44fb7dbb6101a03c57a74f9daf94f2
    Merged-In: Icfacc785ab44fb7dbb6101a03c57a74f9daf94f2
    (cherry picked from commit 26b077937e631f33afff3888d7c87c008ddac220)

--- nn/tools/systrace_parser/parser/test/large_test.sh
+++ nn/tools/systrace_parser/parser/test/large_test.sh
@@ -6,6 +6,7 @@ cd $(dirname $(realpath $0))
 ../../parse_systrace.py cpu.html > cpu.tmp
 ../../parse_systrace.py unittest.html > unittest.tmp
 ../../parse_systrace.py --per-execution hdrnet.html > hdrnet-bm.tmp
+../../parse_systrace.py --per-execution --json omr1_incomplete.html > omr1.tmp
 
 echo hdrnet
 diff hdrnet.txt hdrnet.tmp
@@ -15,5 +16,7 @@ echo unittest
 diff unittest.txt unittest.tmp
 echo benchmark
 diff hdrnet-bm.txt hdrnet-bm.tmp
+echo omr1_incomplete
+diff omr1.json omr1.tmp
 
 echo Success
