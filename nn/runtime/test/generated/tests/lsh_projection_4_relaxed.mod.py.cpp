5d941eb3698e0fdc34afd38081a421bca32b135a
Michael K. Sanders <mks@google.com>
2018-09-10 16:47:53 +0100

    Updates LSH_PROJECTION to match change in tflite.
    Bug: 111751836
    Test: Ran NeuralNetworksTest_static on device.
    Change-Id: I14dce4a8541fe468d603a9032818857770462ff8
    Merged-In: I14dce4a8541fe468d603a9032818857770462ff8
    (cherry picked from commit 9b9228bedbf5a24f96ddd36c5d8a12d983d43c83)

---
+++ nn/runtime/test/generated/tests/lsh_projection_4_relaxed.mod.py.cpp
@@ -0,0 +1,17 @@
+// clang-format off
+// Generated file (from: lsh_projection_4_relaxed.mod.py). Do not edit
+#include "../../TestGenerated.h"
+
+namespace lsh_projection_4_relaxed {
+// Generated lsh_projection_4_relaxed test
+#include "generated/examples/lsh_projection_4_relaxed.example.cpp"
+// Generated model constructor
+#include "generated/models/lsh_projection_4_relaxed.model.cpp"
+} // namespace lsh_projection_4_relaxed
+
+TEST_F(GeneratedTests, lsh_projection_4_relaxed) {
+    execute(lsh_projection_4_relaxed::CreateModel,
+            lsh_projection_4_relaxed::is_ignored,
+            lsh_projection_4_relaxed::examples);
+}
+
