331cbdde8bb0a839e6a806489f0aea008b461817
Michael K. Sanders <mks@google.com>
2018-09-10 16:47:53 +0100

    Updates LSH_PROJECTION to match change in tflite.
    Bug: 111751836
    Test: Ran NeuralNetworksTest_static on device.
    Change-Id: I14dce4a8541fe468d603a9032818857770462ff8

---
+++ runtime/test/generated/examples/lsh_projection_4_relaxed.example.cpp
@@ -0,0 +1,26 @@
+// clang-format off
+// Generated file (from: lsh_projection_4_relaxed.mod.py). Do not edit
+std::vector<MixedTypedExample> examples = {
+// Begin of an example
+{
+//Input(s)
+{ // See tools/test_generator/include/TestHarness.h:MixedTyped
+  // int -> FLOAT32 map
+  {{1, {}}},
+  // int -> INT32 map
+  {{0, {12345, 54321, 67890, 9876, -12345678, -87654321}}},
+  // int -> QUANT8_ASYMM map
+  {}
+},
+//Output(s)
+{ // See tools/test_generator/include/TestHarness.h:MixedTyped
+  // int -> FLOAT32 map
+  {},
+  // int -> INT32 map
+  {{0, {1, 2, 2, 0}}},
+  // int -> QUANT8_ASYMM map
+  {}
+}
+}, // End of an example
+};
+
