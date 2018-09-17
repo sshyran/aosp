cc700116c959e4d2f4a0f1f2bfd9fa13824339d7
Slava Shklyaev <slavash@google.com>
2018-09-17 11:58:40 +0100

    Add new ops ARGMAX and ARGMIN
    Note that I could not name it ARG_MAX because of a name collision with a
    macro in libc/kernel/uapi/linux/limits.h.
    
    Bug: 113558658
    Test: NeuralNetworksTest_static
    Change-Id: Ibb1cd619d1a5428ab4bab63412b21f5a63ac2cd5

---
+++ runtime/test/generated/examples/argmax_1_int32.example.cpp
@@ -0,0 +1,22 @@
+// Generated file (from: argmax_1_int32.mod.py). Do not edit
+// Begin of an example
+{
+//Input(s)
+{ // See tools/test_generator/include/TestHarness.h:MixedTyped
+  // int -> FLOAT32 map
+  {},
+  // int -> INT32 map
+  {{0, {1, 2, 4, 3}}},
+  // int -> QUANT8_ASYMM map
+  {}
+},
+//Output(s)
+{ // See tools/test_generator/include/TestHarness.h:MixedTyped
+  // int -> FLOAT32 map
+  {},
+  // int -> INT32 map
+  {{0, {1, 0}}},
+  // int -> QUANT8_ASYMM map
+  {}
+}
+}, // End of an example
