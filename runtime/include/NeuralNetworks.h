53c72aa4bf20eab6191411fc1c83937da856f092
Xusong Wang <xusongw@google.com>
2018-09-25 18:52:25 -0700

    Add NCHW data layout support to AVERAGE_POOL_2D.
    Add an optional parameter data_layout to AVERAGE_POOL_2D op for NCHW
    data layout support. Create testcases and generate test.
    
    Provide reference CPU implementation for NCHW support. The native layout
    for CPU implementation is still NHWC. Appropriate transpositions are
    applied for NCHW input/output.
    
    Bug: 112320705
    
    Test: NeuralNetworksTest_static
    Change-Id: I8684bd22697d71666f26b5433ddf48839476f496
    Merged-In: I8684bd22697d71666f26b5433ddf48839476f496
    (cherry picked from commit 904320ffc615f60461a1a556a5463cebaae64e92)

--- runtime/include/NeuralNetworks.h
+++ runtime/include/NeuralNetworks.h
@@ -163,8 +163,10 @@ typedef enum {
      * * {@link ANEURALNETWORKS_TENSOR_FLOAT32}
      * * {@link ANEURALNETWORKS_TENSOR_QUANT8_ASYMM}
      *
-     * Supported tensor rank: 4, with "NHWC" (i.e., Num_samples, Height, Width,
-     * and Channels) data layout.
+     * Supported tensor rank: 4, with "NHWC" or "NCHW" data layout.
+     * With the default data layout NHWC, the data is stored in the order of:
+     * [batch, height, width, channels]. Alternatively, the data layout could
+     * be NCHW, the data storage order of: [batch, channels, height, width].
      *
      * Both explicit padding and implicit padding are supported.
      *
@@ -190,6 +192,8 @@ typedef enum {
      * * 9: An {@link ANEURALNETWORKS_INT32} scalar, and has to be one of the
      *      {@link FuseCode} values. Specifies the activation to
      *      invoke on the result.
+     * * 10: An optional {@link ANEURALNETWORKS_BOOL} scalar, default to false.
+     *       Set to true to specify NCHW data layout for input0 and output0.
      *
      * Inputs (implicit padding):
      * * 0: A 4-D tensor, of shape [batches, height, width, depth], specifying
@@ -208,6 +212,8 @@ typedef enum {
      * * 6: An {@link ANEURALNETWORKS_INT32} scalar, and has to be one of the
      *      {@link FuseCode} values. Specifies the activation to
      *      invoke on the result.
+     * * 7: An optional {@link ANEURALNETWORKS_BOOL} scalar, default to false.
+     *      Set to true to specify NCHW data layout for input0 and output0.
      *
      * Outputs:
      * * 0: The output 4-D tensor, of shape
