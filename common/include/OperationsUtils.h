9057e9f93db653b4fc27b4008a33692c2fca0276
Slava Shklyaev <slavash@google.com>
2018-09-27 15:13:47 +0100

    Add new op EXPAND_DIMS
    Bug: 113561888
    Test: NeuralNetworksTest_static
    Change-Id: If7b64a16dfc4d4a821a723a94ffff301956ca557
    Merged-In: If7b64a16dfc4d4a821a723a94ffff301956ca557
    (cherry picked from commit 3914e85d362faad8f821286512b4f6b8a90d2f20)

--- common/include/OperationsUtils.h
+++ common/include/OperationsUtils.h
@@ -72,6 +72,7 @@ uint32_t getSizeOfDimension(const Shape& shape, uint32_t dimensionIdx);
 
 // Converts an axis index from the range [-dims, dims) into the range [0, dims).
 int32_t getDimensionIndex(const Shape& shape, int32_t axis);
+int32_t getDimensionIndex(int32_t numberOfDimensions, int32_t axis);
 
 inline uint32_t computeOutSize(uint32_t imageSize, uint32_t filterSize, uint32_t stride,
                                uint32_t paddingHead, uint32_t paddingTail) {
---
+++ runtime/test/generated/tests/expand_dims.mod.py.cpp
@@ -0,0 +1,107 @@
+// clang-format off
+// Generated file (from: expand_dims.mod.py). Do not edit
+#include "../../TestGenerated.h"
+
+namespace expand_dims {
+// Generated expand_dims test
+#include "generated/examples/expand_dims.example.cpp"
+// Generated model constructor
+#include "generated/models/expand_dims.model.cpp"
+} // namespace expand_dims
+
+TEST_F(GeneratedTests, expand_dims) {
+    execute(expand_dims::CreateModel,
+            expand_dims::is_ignored,
+            expand_dims::examples);
+}
+
+TEST_F(GeneratedTests, expand_dims_relaxed) {
+    execute(expand_dims::CreateModel_relaxed,
+            expand_dims::is_ignored_relaxed,
+            expand_dims::examples_relaxed);
+}
+
+TEST_F(GeneratedTests, expand_dims_quant8) {
+    execute(expand_dims::CreateModel_quant8,
+            expand_dims::is_ignored_quant8,
+            expand_dims::examples_quant8);
+}
+
+TEST_F(GeneratedTests, expand_dims_int32) {
+    execute(expand_dims::CreateModel_int32,
+            expand_dims::is_ignored_int32,
+            expand_dims::examples_int32);
+}
+
+TEST_F(GeneratedTests, expand_dims_2) {
+    execute(expand_dims::CreateModel_2,
+            expand_dims::is_ignored_2,
+            expand_dims::examples_2);
+}
+
+TEST_F(GeneratedTests, expand_dims_relaxed_2) {
+    execute(expand_dims::CreateModel_relaxed_2,
+            expand_dims::is_ignored_relaxed_2,
+            expand_dims::examples_relaxed_2);
+}
+
+TEST_F(GeneratedTests, expand_dims_quant8_2) {
+    execute(expand_dims::CreateModel_quant8_2,
+            expand_dims::is_ignored_quant8_2,
+            expand_dims::examples_quant8_2);
+}
+
+TEST_F(GeneratedTests, expand_dims_int32_2) {
+    execute(expand_dims::CreateModel_int32_2,
+            expand_dims::is_ignored_int32_2,
+            expand_dims::examples_int32_2);
+}
+
+TEST_F(GeneratedTests, expand_dims_3) {
+    execute(expand_dims::CreateModel_3,
+            expand_dims::is_ignored_3,
+            expand_dims::examples_3);
+}
+
+TEST_F(GeneratedTests, expand_dims_relaxed_3) {
+    execute(expand_dims::CreateModel_relaxed_3,
+            expand_dims::is_ignored_relaxed_3,
+            expand_dims::examples_relaxed_3);
+}
+
+TEST_F(GeneratedTests, expand_dims_quant8_3) {
+    execute(expand_dims::CreateModel_quant8_3,
+            expand_dims::is_ignored_quant8_3,
+            expand_dims::examples_quant8_3);
+}
+
+TEST_F(GeneratedTests, expand_dims_int32_3) {
+    execute(expand_dims::CreateModel_int32_3,
+            expand_dims::is_ignored_int32_3,
+            expand_dims::examples_int32_3);
+}
+
+TEST_F(GeneratedTests, expand_dims_4) {
+    execute(expand_dims::CreateModel_4,
+            expand_dims::is_ignored_4,
+            expand_dims::examples_4);
+}
+
+TEST_F(GeneratedTests, expand_dims_relaxed_4) {
+    execute(expand_dims::CreateModel_relaxed_4,
+            expand_dims::is_ignored_relaxed_4,
+            expand_dims::examples_relaxed_4);
+}
+
+TEST_F(GeneratedTests, expand_dims_quant8_4) {
+    execute(expand_dims::CreateModel_quant8_4,
+            expand_dims::is_ignored_quant8_4,
+            expand_dims::examples_quant8_4);
+}
+
+TEST_F(GeneratedTests, expand_dims_int32_4) {
+    execute(expand_dims::CreateModel_int32_4,
+            expand_dims::is_ignored_int32_4,
+            expand_dims::examples_int32_4);
+}
+
