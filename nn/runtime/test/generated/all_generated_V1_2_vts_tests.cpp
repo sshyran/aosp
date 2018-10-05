a9caa1a3db1b3310920d660007357d572fa52401
Slava Shklyaev <slavash@google.com>
2018-10-05 14:42:28 +0100

    Add new op QUANTIZE
    Fix: 113563338
    Test: NeuralNetworksTest_static
    Change-Id: I98036109c998513595463a7fc5c453f2c8b9acf3

--- nn/runtime/test/generated/all_generated_V1_2_vts_tests.cpp
+++ nn/runtime/test/generated/all_generated_V1_2_vts_tests.cpp
@@ -751,6 +751,42 @@ TEST_F(NeuralnetworksHidlTest, prelu_weight_as_input_quant8) {
                            prelu::examples_weight_as_input_quant8);
 }
 
+// Generated from: quantize.mod.py.
+namespace quantize {
+// Generated quantize test
+#include "examples/quantize.example.cpp"
+// Generated model constructor
+#include "vts_models/quantize.model.cpp"
+} // namespace quantize
+
+TEST_F(NeuralnetworksHidlTest, quantize_quant8) {
+  generated_tests::Execute(device,
+                           quantize::createTestModel_quant8,
+                           quantize::is_ignored_quant8,
+                           quantize::examples_quant8);
+}
+
+TEST_F(NeuralnetworksHidlTest, quantize_quant8_2) {
+  generated_tests::Execute(device,
+                           quantize::createTestModel_quant8_2,
+                           quantize::is_ignored_quant8_2,
+                           quantize::examples_quant8_2);
+}
+
+TEST_F(NeuralnetworksHidlTest, quantize_quant8_3) {
+  generated_tests::Execute(device,
+                           quantize::createTestModel_quant8_3,
+                           quantize::is_ignored_quant8_3,
+                           quantize::examples_quant8_3);
+}
+
+TEST_F(NeuralnetworksHidlTest, quantize_quant8_4) {
+  generated_tests::Execute(device,
+                           quantize::createTestModel_quant8_4,
+                           quantize::is_ignored_quant8_4,
+                           quantize::examples_quant8_4);
+}
+
 // Generated from: roi_align.mod.py.
 namespace roi_align {
 // Generated roi_align test
