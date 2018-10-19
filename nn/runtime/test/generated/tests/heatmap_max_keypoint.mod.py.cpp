7452abb885f5ee9cb1d36dcbfe19ebd91db433cd
Xusong Wang <xusongw@google.com>
2018-10-19 16:21:14 -0700

    Add NCHW data layout support to HEATMAP_MAX_KEYPOINT.
    Add a parameter data_layout to HEATMAP_MAX_KEYPOINT op for NCHW
    data layout support. Create testcases and generate test.
    
    Provide reference CPU implementation for NCHW support. The native layout
    for CPU implementation is still NHWC. Appropriate transpositions are
    applied for NCHW input/output.
    
    Also, fix the bug in output channel order.
    
    Bug: 113562572
    
    Test: NeuralNetworksTest_static
    Change-Id: I4c386df200827e949cf4a34862fc8d1fbdb909e7

--- nn/runtime/test/generated/tests/heatmap_max_keypoint.mod.py.cpp
+++ nn/runtime/test/generated/tests/heatmap_max_keypoint.mod.py.cpp
@@ -9,27 +9,51 @@ namespace heatmap_max_keypoint {
 #include "generated/models/heatmap_max_keypoint.model.cpp"
 } // namespace heatmap_max_keypoint
 
-TEST_F(GeneratedTests, heatmap_max_keypoint) {
-    execute(heatmap_max_keypoint::CreateModel,
-            heatmap_max_keypoint::is_ignored,
-            heatmap_max_keypoint::examples);
+TEST_F(GeneratedTests, heatmap_max_keypoint_nhwc) {
+    execute(heatmap_max_keypoint::CreateModel_nhwc,
+            heatmap_max_keypoint::is_ignored_nhwc,
+            heatmap_max_keypoint::examples_nhwc);
 }
 
-TEST_F(GeneratedTests, heatmap_max_keypoint_relaxed) {
-    execute(heatmap_max_keypoint::CreateModel_relaxed,
-            heatmap_max_keypoint::is_ignored_relaxed,
-            heatmap_max_keypoint::examples_relaxed);
+TEST_F(GeneratedTests, heatmap_max_keypoint_nhwc_relaxed) {
+    execute(heatmap_max_keypoint::CreateModel_nhwc_relaxed,
+            heatmap_max_keypoint::is_ignored_nhwc_relaxed,
+            heatmap_max_keypoint::examples_nhwc_relaxed);
 }
 
-TEST_F(GeneratedTests, heatmap_max_keypoint_2) {
-    execute(heatmap_max_keypoint::CreateModel_2,
-            heatmap_max_keypoint::is_ignored_2,
-            heatmap_max_keypoint::examples_2);
+TEST_F(GeneratedTests, heatmap_max_keypoint_nchw) {
+    execute(heatmap_max_keypoint::CreateModel_nchw,
+            heatmap_max_keypoint::is_ignored_nchw,
+            heatmap_max_keypoint::examples_nchw);
 }
 
-TEST_F(GeneratedTests, heatmap_max_keypoint_relaxed_2) {
-    execute(heatmap_max_keypoint::CreateModel_relaxed_2,
-            heatmap_max_keypoint::is_ignored_relaxed_2,
-            heatmap_max_keypoint::examples_relaxed_2);
+TEST_F(GeneratedTests, heatmap_max_keypoint_nchw_relaxed) {
+    execute(heatmap_max_keypoint::CreateModel_nchw_relaxed,
+            heatmap_max_keypoint::is_ignored_nchw_relaxed,
+            heatmap_max_keypoint::examples_nchw_relaxed);
+}
+
+TEST_F(GeneratedTests, heatmap_max_keypoint_nhwc_2) {
+    execute(heatmap_max_keypoint::CreateModel_nhwc_2,
+            heatmap_max_keypoint::is_ignored_nhwc_2,
+            heatmap_max_keypoint::examples_nhwc_2);
+}
+
+TEST_F(GeneratedTests, heatmap_max_keypoint_nhwc_relaxed_2) {
+    execute(heatmap_max_keypoint::CreateModel_nhwc_relaxed_2,
+            heatmap_max_keypoint::is_ignored_nhwc_relaxed_2,
+            heatmap_max_keypoint::examples_nhwc_relaxed_2);
+}
+
+TEST_F(GeneratedTests, heatmap_max_keypoint_nchw_2) {
+    execute(heatmap_max_keypoint::CreateModel_nchw_2,
+            heatmap_max_keypoint::is_ignored_nchw_2,
+            heatmap_max_keypoint::examples_nchw_2);
+}
+
+TEST_F(GeneratedTests, heatmap_max_keypoint_nchw_relaxed_2) {
+    execute(heatmap_max_keypoint::CreateModel_nchw_relaxed_2,
+            heatmap_max_keypoint::is_ignored_nchw_relaxed_2,
+            heatmap_max_keypoint::examples_nchw_relaxed_2);
 }
 
