1ac959f908dfa2cbc59d60f10d3f4a56cde89cb9
Michael K. Sanders <mks@google.com>
2018-10-29 12:02:39 +0000

    Adds float16 support for ADD operation.
    Test: NeuralNetworksTest_static
    Test: VtsHalNeuralnetworksV1_2TargetTest --hal_service_instance=android.hardware.neuralnetworks@1.2::IDevice/sample-all --gtest_filter=ValidationTest.add_float16
    Bug: 118607838
    Change-Id: I0b8b40d7a2f967d7c3e185466410299eee5d3463

---
+++ runtime/test/generated/tests/add_float16.mod.py.cpp
@@ -0,0 +1,17 @@
+// clang-format off
+// Generated file (from: add_float16.mod.py). Do not edit
+#include "../../TestGenerated.h"
+
+namespace add_float16 {
+// Generated add_float16 test
+#include "generated/examples/add_float16.example.cpp"
+// Generated model constructor
+#include "generated/models/add_float16.model.cpp"
+} // namespace add_float16
+
+TEST_F(GeneratedTests, add_float16) {
+    execute(add_float16::CreateModel,
+            add_float16::is_ignored,
+            add_float16::examples);
+}
+
