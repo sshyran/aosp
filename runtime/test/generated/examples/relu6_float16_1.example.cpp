26b0be0c6e28397699bb2d8d8ba2686e82e46fba
Michael K. Sanders <mks@google.com>
2018-11-15 10:57:01 +0000

    Adds float16 support for RELU*.
    Bug: 118608103
    Bug: 118608488
    Bug: 118608177
    Test: NeuralNetworksTest_static --gtest_filter="GeneratedTests.*float16*"
    Change-Id: I07d927a99ab788b1d79df2defb4e7ff71203fd4d
    Merged-In: I07d927a99ab788b1d79df2defb4e7ff71203fd4d
    (cherry picked from commit 2de2f51f4cbe6a7b14193da03d7c7a6b809fa8d7)

---
+++ runtime/test/generated/examples/relu6_float16_1.example.cpp
@@ -0,0 +1,36 @@
+// clang-format off
+// Generated file (from: relu6_float16_1.mod.py). Do not edit
+std::vector<MixedTypedExample> examples = {
+// Begin of an example
+{
+.operands = {
+//Input(s)
+{ // See tools/test_generator/include/TestHarness.h:MixedTyped
+  // int -> FLOAT32 map
+  {{0, {-10.0f, -0.5f, 0.5f, 10.0f}}},
+  // int -> INT32 map
+  {},
+  // int -> QUANT8_ASYMM map
+  {},
+  // int -> QUANT16_ASYMM map
+  {},
+  // int -> FLOAT16 map
+  {},
+},
+//Output(s)
+{ // See tools/test_generator/include/TestHarness.h:MixedTyped
+  // int -> FLOAT32 map
+  {{0, {0.0f, 0.0f, 0.5f, 6.0f}}},
+  // int -> INT32 map
+  {},
+  // int -> QUANT8_ASYMM map
+  {},
+  // int -> QUANT16_ASYMM map
+  {},
+  // int -> FLOAT16 map
+  {},
+}
+},
+}, // End of an example
+};
+
