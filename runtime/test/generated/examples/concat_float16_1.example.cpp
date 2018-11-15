ece0aadfae1792a7d4e625cf3cfd1f9afa5f8fda
Michael K. Sanders <mks@google.com>
2018-11-15 22:21:59 +0000

    Adds float16 support for CONCATENATION.
    Bug: 118608022
    Test: NeuralNetworksTest_static --gtest_filter="GeneratedTests.*"
    Change-Id: Ie9601b31d0c1826f7cee6caa5bae34adc07c3527

---
+++ runtime/test/generated/examples/concat_float16_1.example.cpp
@@ -0,0 +1,36 @@
+// clang-format off
+// Generated file (from: concat_float16_1.mod.py). Do not edit
+std::vector<MixedTypedExample> examples = {
+// Begin of an example
+{
+.operands = {
+//Input(s)
+{ // See tools/test_generator/include/TestHarness.h:MixedTyped
+  // int -> FLOAT32 map
+  {},
+  // int -> INT32 map
+  {},
+  // int -> QUANT8_ASYMM map
+  {},
+  // int -> QUANT16_ASYMM map
+  {},
+  // int -> FLOAT16 map
+  {{0, {1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f}}, {1, {7.0f, 8.0f, 9.0f, 10.0f, 11.0f, 12.0f}}},
+},
+//Output(s)
+{ // See tools/test_generator/include/TestHarness.h:MixedTyped
+  // int -> FLOAT32 map
+  {},
+  // int -> INT32 map
+  {},
+  // int -> QUANT8_ASYMM map
+  {},
+  // int -> QUANT16_ASYMM map
+  {},
+  // int -> FLOAT16 map
+  {{0, {1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f, 11.0f, 12.0f}}},
+}
+},
+}, // End of an example
+};
+
