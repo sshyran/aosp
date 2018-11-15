63e8783ed26907859194d057b4967ace4975fce9
Slava Shklyaev <slavash@google.com>
2018-11-15 12:13:36 +0000

    Add new op ABS
    Change-Id: I484bd5c896bc575853133fb89564c6f2df89bda0
    Merged-In: I484bd5c896bc575853133fb89564c6f2df89bda0
    Fix: 115976509
    Fix: 118609009
    Test: NeuralNetworksTest_static
    (cherry picked from commit 2412f257a5a8ec697b294030e8567ffad21da24a)

---
+++ nn/runtime/test/generated/tests/abs.mod.py.cpp
@@ -0,0 +1,29 @@
+// clang-format off
+// Generated file (from: abs.mod.py). Do not edit
+#include "../../TestGenerated.h"
+
+namespace abs {
+// Generated abs test
+#include "generated/examples/abs.example.cpp"
+// Generated model constructor
+#include "generated/models/abs.model.cpp"
+} // namespace abs
+
+TEST_F(GeneratedTests, abs) {
+    execute(abs::CreateModel,
+            abs::is_ignored,
+            abs::examples);
+}
+
+TEST_F(GeneratedTests, abs_relaxed) {
+    execute(abs::CreateModel_relaxed,
+            abs::is_ignored_relaxed,
+            abs::examples_relaxed);
+}
+
+TEST_F(GeneratedTests, abs_float16) {
+    execute(abs::CreateModel_float16,
+            abs::is_ignored_float16,
+            abs::examples_float16);
+}
+
