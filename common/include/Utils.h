88fe2437673dab80cef6bccb60f23e02ddbdb15f
Slava Shklyaev <slavash@google.com>
2018-11-30 22:50:15 +0000

    Add REDUCE_* ops
    Operations added:
    - REDUCE_PROD
    - REDUCE_SUM
    - REDUCE_MAX
    - REDUCE_MIN
    - REDUCE_ANY
    - REDUCE_ALL
    
    Fix: 113564646
    Test: NeuralNetworksTest_static_asan
    Change-Id: I3a03358c941b0d0b324406b4f43a8bde97dfa505

--- common/include/Utils.h
+++ common/include/Utils.h
@@ -31,7 +31,7 @@ namespace nn {
 const int kNumberOfDataTypes = 11;
 
 // The number of operation types (OperationCode) defined in NeuralNetworks.h.
-const int kNumberOfOperationTypes = 92;
+const int kNumberOfOperationTypes = 97;
 
 // The number of execution preferences defined in NeuralNetworks.h.
 const int kNumberOfPreferences = 3;
--- runtime/test/TestValidateOperations.cpp
+++ runtime/test/TestValidateOperations.cpp
@@ -1953,4 +1953,69 @@ TEST(OperationValidationTest, GREATER_EQUAL) {
     comparisonTest(ANEURALNETWORKS_GREATER_EQUAL, ANEURALNETWORKS_TENSOR_QUANT8_ASYMM);
 }
 
+void reduceOpTest(ANeuralNetworksOperationType operationCode, int32_t inputOperandType) {
+    bool isQuant = inputOperandType == ANEURALNETWORKS_TENSOR_QUANT8_ASYMM;
+    float scale = isQuant ? 1.f / 256 : 0.0f;
+    uint32_t inputDimensions[4] = {2, 2, 2, 2};
+    ANeuralNetworksOperandType input1 = {
+            .type = inputOperandType,
+            .dimensionCount = 4,
+            .dimensions = inputDimensions,
+            .scale = scale,
+            .zeroPoint = 0,
+    };
+    uint32_t axesDimensions[1] = {2};
+    ANeuralNetworksOperandType input2 = {
+            .type = ANEURALNETWORKS_TENSOR_INT32,
+            .dimensionCount = 1,
+            .dimensions = axesDimensions,
+    };
+    ANeuralNetworksOperandType input3 = {
+            .type = ANEURALNETWORKS_BOOL,
+            .dimensions = {},
+    };
+    ANeuralNetworksOperandType output = {
+            .type = inputOperandType,
+            .dimensionCount = 4,
+            .dimensions = inputDimensions,
+            .scale = scale,
+    };
+    OperationTestBase test(operationCode, {input1, input2, input3}, {output});
+
+    EXPECT_TRUE(test.testMutatingInputOperandCode());
+    EXPECT_TRUE(test.testMutatingInputOperandCounts());
+    EXPECT_TRUE(test.testMutatingOutputOperandCode());
+    EXPECT_TRUE(test.testMutatingOutputOperandCounts());
+}
+
+TEST(OperationValidationTest, REDUCE_PROD) {
+    reduceOpTest(ANEURALNETWORKS_REDUCE_PROD, ANEURALNETWORKS_TENSOR_FLOAT16);
+    reduceOpTest(ANEURALNETWORKS_REDUCE_PROD, ANEURALNETWORKS_TENSOR_FLOAT32);
+}
+
+TEST(OperationValidationTest, REDUCE_SUM) {
+    reduceOpTest(ANEURALNETWORKS_REDUCE_SUM, ANEURALNETWORKS_TENSOR_FLOAT16);
+    reduceOpTest(ANEURALNETWORKS_REDUCE_SUM, ANEURALNETWORKS_TENSOR_FLOAT32);
+}
+
+TEST(OperationValidationTest, REDUCE_MAX) {
+    reduceOpTest(ANEURALNETWORKS_REDUCE_MAX, ANEURALNETWORKS_TENSOR_FLOAT16);
+    reduceOpTest(ANEURALNETWORKS_REDUCE_MAX, ANEURALNETWORKS_TENSOR_FLOAT32);
+    reduceOpTest(ANEURALNETWORKS_REDUCE_MAX, ANEURALNETWORKS_TENSOR_QUANT8_ASYMM);
+}
+
+TEST(OperationValidationTest, REDUCE_MIN) {
+    reduceOpTest(ANEURALNETWORKS_REDUCE_MIN, ANEURALNETWORKS_TENSOR_FLOAT16);
+    reduceOpTest(ANEURALNETWORKS_REDUCE_MIN, ANEURALNETWORKS_TENSOR_FLOAT32);
+    reduceOpTest(ANEURALNETWORKS_REDUCE_MIN, ANEURALNETWORKS_TENSOR_QUANT8_ASYMM);
+}
+
+TEST(OperationValidationTest, REDUCE_ANY) {
+    reduceOpTest(ANEURALNETWORKS_REDUCE_ANY, ANEURALNETWORKS_TENSOR_BOOL8);
+}
+
+TEST(OperationValidationTest, REDUCE_ALL) {
+    reduceOpTest(ANEURALNETWORKS_REDUCE_ALL, ANEURALNETWORKS_TENSOR_BOOL8);
+}
+
 }  // end namespace
