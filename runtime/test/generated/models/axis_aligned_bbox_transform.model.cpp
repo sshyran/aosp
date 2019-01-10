bd5dd707338130f6b1bc41948f5e5f20af8edb34
Xusong Wang <xusongw@google.com>
2019-01-09 18:37:28 -0800

    Regenerate tests.
    Regenerate tests by first clear generated/ directory and then run
    generate_test.sh & generate_vts_test.sh.
    
    Test: NeuralNetworksTest_static
    Change-Id: I8756b967e954639b28780cd50000c31f66c8e427
    Merged-In: I8756b967e954639b28780cd50000c31f66c8e427
    (cherry picked from commit 43604e26b96b86ee8c017f200b21081e070e877b)

--- runtime/test/generated/models/axis_aligned_bbox_transform.model.cpp
+++ runtime/test/generated/models/axis_aligned_bbox_transform.model.cpp
@@ -1,30 +1,22 @@
 // clang-format off
 // Generated file (from: axis_aligned_bbox_transform.mod.py). Do not edit
 void CreateModel(Model *model) {
-  OperandType type0(Type::TENSOR_FLOAT32, {5, 5});
+  OperandType type0(Type::TENSOR_FLOAT32, {5, 4});
   OperandType type1(Type::TENSOR_FLOAT32, {5, 8});
-  OperandType type2(Type::TENSOR_FLOAT32, {5, 3});
-  OperandType type3(Type::TENSOR_INT32, {5});
-  OperandType type4(Type::TENSOR_FLOAT32, {4});
-  OperandType type5(Type::BOOL, {});
+  OperandType type2(Type::TENSOR_INT32, {4});
+  OperandType type3(Type::TENSOR_FLOAT32, {4, 3});
   // Phase 1, operands
   auto roi = model->addOperand(&type0);
   auto bboxDeltas = model->addOperand(&type1);
-  auto imageInfo = model->addOperand(&type2);
-  auto param = model->addOperand(&type4);
-  auto param1 = model->addOperand(&type5);
+  auto batchSplit = model->addOperand(&type2);
+  auto imageInfo = model->addOperand(&type3);
   auto out = model->addOperand(&type1);
-  auto batchSplit = model->addOperand(&type3);
   // Phase 2, operations
-  static float param_init[] = {2.0f, 2.0f, 0.5f, 0.5f};
-  model->setOperandValue(param, param_init, sizeof(float) * 4);
-  static bool8 param1_init[] = {false};
-  model->setOperandValue(param1, param1_init, sizeof(bool8) * 1);
-  model->addOperation(ANEURALNETWORKS_AXIS_ALIGNED_BBOX_TRANSFORM, {roi, bboxDeltas, imageInfo, param, param1}, {out, batchSplit});
+  model->addOperation(ANEURALNETWORKS_AXIS_ALIGNED_BBOX_TRANSFORM, {roi, bboxDeltas, batchSplit, imageInfo}, {out});
   // Phase 3, inputs and outputs
   model->identifyInputsAndOutputs(
-    {roi, bboxDeltas, imageInfo},
-    {out, batchSplit});
+    {roi, bboxDeltas, batchSplit, imageInfo},
+    {out});
   assert(model->isValid());
 }
 
@@ -34,30 +26,22 @@ inline bool is_ignored(int i) {
 }
 
 void CreateModel_relaxed(Model *model) {
-  OperandType type0(Type::TENSOR_FLOAT32, {5, 5});
+  OperandType type0(Type::TENSOR_FLOAT32, {5, 4});
   OperandType type1(Type::TENSOR_FLOAT32, {5, 8});
-  OperandType type2(Type::TENSOR_FLOAT32, {5, 3});
-  OperandType type3(Type::TENSOR_INT32, {5});
-  OperandType type4(Type::TENSOR_FLOAT32, {4});
-  OperandType type5(Type::BOOL, {});
+  OperandType type2(Type::TENSOR_INT32, {4});
+  OperandType type3(Type::TENSOR_FLOAT32, {4, 3});
   // Phase 1, operands
   auto roi = model->addOperand(&type0);
   auto bboxDeltas = model->addOperand(&type1);
-  auto imageInfo = model->addOperand(&type2);
-  auto param = model->addOperand(&type4);
-  auto param1 = model->addOperand(&type5);
+  auto batchSplit = model->addOperand(&type2);
+  auto imageInfo = model->addOperand(&type3);
   auto out = model->addOperand(&type1);
-  auto batchSplit = model->addOperand(&type3);
   // Phase 2, operations
-  static float param_init[] = {2.0f, 2.0f, 0.5f, 0.5f};
-  model->setOperandValue(param, param_init, sizeof(float) * 4);
-  static bool8 param1_init[] = {false};
-  model->setOperandValue(param1, param1_init, sizeof(bool8) * 1);
-  model->addOperation(ANEURALNETWORKS_AXIS_ALIGNED_BBOX_TRANSFORM, {roi, bboxDeltas, imageInfo, param, param1}, {out, batchSplit});
+  model->addOperation(ANEURALNETWORKS_AXIS_ALIGNED_BBOX_TRANSFORM, {roi, bboxDeltas, batchSplit, imageInfo}, {out});
   // Phase 3, inputs and outputs
   model->identifyInputsAndOutputs(
-    {roi, bboxDeltas, imageInfo},
-    {out, batchSplit});
+    {roi, bboxDeltas, batchSplit, imageInfo},
+    {out});
   // Phase 4: set relaxed execution
   model->relaxComputationFloat32toFloat16(true);
   assert(model->isValid());
@@ -69,30 +53,22 @@ inline bool is_ignored_relaxed(int i) {
 }
 
 void CreateModel_float16(Model *model) {
-  OperandType type10(Type::TENSOR_FLOAT16, {5, 8});
-  OperandType type11(Type::TENSOR_FLOAT16, {5, 3});
-  OperandType type12(Type::TENSOR_FLOAT16, {4});
-  OperandType type13(Type::TENSOR_FLOAT16, {5, 5});
-  OperandType type3(Type::TENSOR_INT32, {5});
-  OperandType type5(Type::BOOL, {});
+  OperandType type2(Type::TENSOR_INT32, {4});
+  OperandType type6(Type::TENSOR_FLOAT16, {5, 8});
+  OperandType type7(Type::TENSOR_FLOAT16, {4, 3});
+  OperandType type8(Type::TENSOR_FLOAT16, {5, 4});
   // Phase 1, operands
-  auto roi = model->addOperand(&type13);
-  auto bboxDeltas = model->addOperand(&type10);
-  auto imageInfo = model->addOperand(&type11);
-  auto param = model->addOperand(&type12);
-  auto param1 = model->addOperand(&type5);
-  auto out = model->addOperand(&type10);
-  auto batchSplit = model->addOperand(&type3);
+  auto roi = model->addOperand(&type8);
+  auto bboxDeltas = model->addOperand(&type6);
+  auto batchSplit = model->addOperand(&type2);
+  auto imageInfo = model->addOperand(&type7);
+  auto out = model->addOperand(&type6);
   // Phase 2, operations
-  static _Float16 param_init[] = {2.0f, 2.0f, 0.5f, 0.5f};
-  model->setOperandValue(param, param_init, sizeof(_Float16) * 4);
-  static bool8 param1_init[] = {false};
-  model->setOperandValue(param1, param1_init, sizeof(bool8) * 1);
-  model->addOperation(ANEURALNETWORKS_AXIS_ALIGNED_BBOX_TRANSFORM, {roi, bboxDeltas, imageInfo, param, param1}, {out, batchSplit});
+  model->addOperation(ANEURALNETWORKS_AXIS_ALIGNED_BBOX_TRANSFORM, {roi, bboxDeltas, batchSplit, imageInfo}, {out});
   // Phase 3, inputs and outputs
   model->identifyInputsAndOutputs(
-    {roi, bboxDeltas, imageInfo},
-    {out, batchSplit});
+    {roi, bboxDeltas, batchSplit, imageInfo},
+    {out});
   assert(model->isValid());
 }
 
@@ -102,30 +78,22 @@ inline bool is_ignored_float16(int i) {
 }
 
 void CreateModel_2(Model *model) {
-  OperandType type0(Type::TENSOR_FLOAT32, {5, 5});
+  OperandType type0(Type::TENSOR_FLOAT32, {5, 4});
   OperandType type1(Type::TENSOR_FLOAT32, {5, 8});
-  OperandType type2(Type::TENSOR_FLOAT32, {5, 3});
-  OperandType type3(Type::TENSOR_INT32, {5});
-  OperandType type4(Type::TENSOR_FLOAT32, {4});
-  OperandType type5(Type::BOOL, {});
+  OperandType type4(Type::TENSOR_INT32, {7});
+  OperandType type5(Type::TENSOR_FLOAT32, {7, 3});
   // Phase 1, operands
-  auto roi = model->addOperand(&type0);
-  auto bboxDeltas = model->addOperand(&type1);
-  auto imageInfo = model->addOperand(&type2);
-  auto param2 = model->addOperand(&type4);
-  auto param3 = model->addOperand(&type5);
-  auto out = model->addOperand(&type1);
-  auto batchSplit = model->addOperand(&type3);
+  auto roi1 = model->addOperand(&type0);
+  auto bboxDeltas1 = model->addOperand(&type1);
+  auto batchSplit1 = model->addOperand(&type4);
+  auto imageInfo1 = model->addOperand(&type5);
+  auto out1 = model->addOperand(&type1);
   // Phase 2, operations
-  static float param2_init[] = {2.0f, 2.0f, 0.5f, 0.5f};
-  model->setOperandValue(param2, param2_init, sizeof(float) * 4);
-  static bool8 param3_init[] = {true};
-  model->setOperandValue(param3, param3_init, sizeof(bool8) * 1);
-  model->addOperation(ANEURALNETWORKS_AXIS_ALIGNED_BBOX_TRANSFORM, {roi, bboxDeltas, imageInfo, param2, param3}, {out, batchSplit});
+  model->addOperation(ANEURALNETWORKS_AXIS_ALIGNED_BBOX_TRANSFORM, {roi1, bboxDeltas1, batchSplit1, imageInfo1}, {out1});
   // Phase 3, inputs and outputs
   model->identifyInputsAndOutputs(
-    {roi, bboxDeltas, imageInfo},
-    {out, batchSplit});
+    {roi1, bboxDeltas1, batchSplit1, imageInfo1},
+    {out1});
   assert(model->isValid());
 }
 
@@ -135,30 +103,22 @@ inline bool is_ignored_2(int i) {
 }
 
 void CreateModel_relaxed_2(Model *model) {
-  OperandType type0(Type::TENSOR_FLOAT32, {5, 5});
+  OperandType type0(Type::TENSOR_FLOAT32, {5, 4});
   OperandType type1(Type::TENSOR_FLOAT32, {5, 8});
-  OperandType type2(Type::TENSOR_FLOAT32, {5, 3});
-  OperandType type3(Type::TENSOR_INT32, {5});
-  OperandType type4(Type::TENSOR_FLOAT32, {4});
-  OperandType type5(Type::BOOL, {});
+  OperandType type4(Type::TENSOR_INT32, {7});
+  OperandType type5(Type::TENSOR_FLOAT32, {7, 3});
   // Phase 1, operands
-  auto roi = model->addOperand(&type0);
-  auto bboxDeltas = model->addOperand(&type1);
-  auto imageInfo = model->addOperand(&type2);
-  auto param2 = model->addOperand(&type4);
-  auto param3 = model->addOperand(&type5);
-  auto out = model->addOperand(&type1);
-  auto batchSplit = model->addOperand(&type3);
+  auto roi1 = model->addOperand(&type0);
+  auto bboxDeltas1 = model->addOperand(&type1);
+  auto batchSplit1 = model->addOperand(&type4);
+  auto imageInfo1 = model->addOperand(&type5);
+  auto out1 = model->addOperand(&type1);
   // Phase 2, operations
-  static float param2_init[] = {2.0f, 2.0f, 0.5f, 0.5f};
-  model->setOperandValue(param2, param2_init, sizeof(float) * 4);
-  static bool8 param3_init[] = {true};
-  model->setOperandValue(param3, param3_init, sizeof(bool8) * 1);
-  model->addOperation(ANEURALNETWORKS_AXIS_ALIGNED_BBOX_TRANSFORM, {roi, bboxDeltas, imageInfo, param2, param3}, {out, batchSplit});
+  model->addOperation(ANEURALNETWORKS_AXIS_ALIGNED_BBOX_TRANSFORM, {roi1, bboxDeltas1, batchSplit1, imageInfo1}, {out1});
   // Phase 3, inputs and outputs
   model->identifyInputsAndOutputs(
-    {roi, bboxDeltas, imageInfo},
-    {out, batchSplit});
+    {roi1, bboxDeltas1, batchSplit1, imageInfo1},
+    {out1});
   // Phase 4: set relaxed execution
   model->relaxComputationFloat32toFloat16(true);
   assert(model->isValid());
@@ -170,135 +130,26 @@ inline bool is_ignored_relaxed_2(int i) {
 }
 
 void CreateModel_float16_2(Model *model) {
-  OperandType type10(Type::TENSOR_FLOAT16, {5, 8});
-  OperandType type11(Type::TENSOR_FLOAT16, {5, 3});
-  OperandType type12(Type::TENSOR_FLOAT16, {4});
-  OperandType type13(Type::TENSOR_FLOAT16, {5, 5});
-  OperandType type3(Type::TENSOR_INT32, {5});
-  OperandType type5(Type::BOOL, {});
+  OperandType type4(Type::TENSOR_INT32, {7});
+  OperandType type6(Type::TENSOR_FLOAT16, {5, 8});
+  OperandType type8(Type::TENSOR_FLOAT16, {5, 4});
+  OperandType type9(Type::TENSOR_FLOAT16, {7, 3});
   // Phase 1, operands
-  auto roi = model->addOperand(&type13);
-  auto bboxDeltas = model->addOperand(&type10);
-  auto imageInfo = model->addOperand(&type11);
-  auto param2 = model->addOperand(&type12);
-  auto param3 = model->addOperand(&type5);
-  auto out = model->addOperand(&type10);
-  auto batchSplit = model->addOperand(&type3);
+  auto roi1 = model->addOperand(&type8);
+  auto bboxDeltas1 = model->addOperand(&type6);
+  auto batchSplit1 = model->addOperand(&type4);
+  auto imageInfo1 = model->addOperand(&type9);
+  auto out1 = model->addOperand(&type6);
   // Phase 2, operations
-  static _Float16 param2_init[] = {2.0f, 2.0f, 0.5f, 0.5f};
-  model->setOperandValue(param2, param2_init, sizeof(_Float16) * 4);
-  static bool8 param3_init[] = {true};
-  model->setOperandValue(param3, param3_init, sizeof(bool8) * 1);
-  model->addOperation(ANEURALNETWORKS_AXIS_ALIGNED_BBOX_TRANSFORM, {roi, bboxDeltas, imageInfo, param2, param3}, {out, batchSplit});
+  model->addOperation(ANEURALNETWORKS_AXIS_ALIGNED_BBOX_TRANSFORM, {roi1, bboxDeltas1, batchSplit1, imageInfo1}, {out1});
   // Phase 3, inputs and outputs
   model->identifyInputsAndOutputs(
-    {roi, bboxDeltas, imageInfo},
-    {out, batchSplit});
+    {roi1, bboxDeltas1, batchSplit1, imageInfo1},
+    {out1});
   assert(model->isValid());
 }
 
 inline bool is_ignored_float16_2(int i) {
-  static std::set<int> ignore = {};
-  return ignore.find(i) != ignore.end();
-}
-
-void CreateModel_single_batch(Model *model) {
-  OperandType type4(Type::TENSOR_FLOAT32, {4});
-  OperandType type5(Type::BOOL, {});
-  OperandType type6(Type::TENSOR_FLOAT32, {2, 4});
-  OperandType type7(Type::TENSOR_FLOAT32, {2, 8});
-  OperandType type8(Type::TENSOR_FLOAT32, {1, 3});
-  OperandType type9(Type::TENSOR_INT32, {1});
-  // Phase 1, operands
-  auto roi1 = model->addOperand(&type6);
-  auto bboxDeltas1 = model->addOperand(&type7);
-  auto imageInfo1 = model->addOperand(&type8);
-  auto param4 = model->addOperand(&type4);
-  auto param5 = model->addOperand(&type5);
-  auto out1 = model->addOperand(&type7);
-  auto batchSplit1 = model->addOperand(&type9);
-  // Phase 2, operations
-  static float param4_init[] = {2.0f, 2.0f, 0.5f, 0.5f};
-  model->setOperandValue(param4, param4_init, sizeof(float) * 4);
-  static bool8 param5_init[] = {true};
-  model->setOperandValue(param5, param5_init, sizeof(bool8) * 1);
-  model->addOperation(ANEURALNETWORKS_AXIS_ALIGNED_BBOX_TRANSFORM, {roi1, bboxDeltas1, imageInfo1, param4, param5}, {out1, batchSplit1});
-  // Phase 3, inputs and outputs
-  model->identifyInputsAndOutputs(
-    {roi1, bboxDeltas1, imageInfo1},
-    {out1, batchSplit1});
-  assert(model->isValid());
-}
-
-inline bool is_ignored_single_batch(int i) {
-  static std::set<int> ignore = {};
-  return ignore.find(i) != ignore.end();
-}
-
-void CreateModel_single_batch_relaxed(Model *model) {
-  OperandType type4(Type::TENSOR_FLOAT32, {4});
-  OperandType type5(Type::BOOL, {});
-  OperandType type6(Type::TENSOR_FLOAT32, {2, 4});
-  OperandType type7(Type::TENSOR_FLOAT32, {2, 8});
-  OperandType type8(Type::TENSOR_FLOAT32, {1, 3});
-  OperandType type9(Type::TENSOR_INT32, {1});
-  // Phase 1, operands
-  auto roi1 = model->addOperand(&type6);
-  auto bboxDeltas1 = model->addOperand(&type7);
-  auto imageInfo1 = model->addOperand(&type8);
-  auto param4 = model->addOperand(&type4);
-  auto param5 = model->addOperand(&type5);
-  auto out1 = model->addOperand(&type7);
-  auto batchSplit1 = model->addOperand(&type9);
-  // Phase 2, operations
-  static float param4_init[] = {2.0f, 2.0f, 0.5f, 0.5f};
-  model->setOperandValue(param4, param4_init, sizeof(float) * 4);
-  static bool8 param5_init[] = {true};
-  model->setOperandValue(param5, param5_init, sizeof(bool8) * 1);
-  model->addOperation(ANEURALNETWORKS_AXIS_ALIGNED_BBOX_TRANSFORM, {roi1, bboxDeltas1, imageInfo1, param4, param5}, {out1, batchSplit1});
-  // Phase 3, inputs and outputs
-  model->identifyInputsAndOutputs(
-    {roi1, bboxDeltas1, imageInfo1},
-    {out1, batchSplit1});
-  // Phase 4: set relaxed execution
-  model->relaxComputationFloat32toFloat16(true);
-  assert(model->isValid());
-}
-
-inline bool is_ignored_single_batch_relaxed(int i) {
-  static std::set<int> ignore = {};
-  return ignore.find(i) != ignore.end();
-}
-
-void CreateModel_single_batch_float16(Model *model) {
-  OperandType type12(Type::TENSOR_FLOAT16, {4});
-  OperandType type14(Type::TENSOR_FLOAT16, {2, 8});
-  OperandType type15(Type::TENSOR_FLOAT16, {1, 3});
-  OperandType type16(Type::TENSOR_FLOAT16, {2, 4});
-  OperandType type5(Type::BOOL, {});
-  OperandType type9(Type::TENSOR_INT32, {1});
-  // Phase 1, operands
-  auto roi1 = model->addOperand(&type16);
-  auto bboxDeltas1 = model->addOperand(&type14);
-  auto imageInfo1 = model->addOperand(&type15);
-  auto param4 = model->addOperand(&type12);
-  auto param5 = model->addOperand(&type5);
-  auto out1 = model->addOperand(&type14);
-  auto batchSplit1 = model->addOperand(&type9);
-  // Phase 2, operations
-  static _Float16 param4_init[] = {2.0f, 2.0f, 0.5f, 0.5f};
-  model->setOperandValue(param4, param4_init, sizeof(_Float16) * 4);
-  static bool8 param5_init[] = {true};
-  model->setOperandValue(param5, param5_init, sizeof(bool8) * 1);
-  model->addOperation(ANEURALNETWORKS_AXIS_ALIGNED_BBOX_TRANSFORM, {roi1, bboxDeltas1, imageInfo1, param4, param5}, {out1, batchSplit1});
-  // Phase 3, inputs and outputs
-  model->identifyInputsAndOutputs(
-    {roi1, bboxDeltas1, imageInfo1},
-    {out1, batchSplit1});
-  assert(model->isValid());
-}
-
-inline bool is_ignored_single_batch_float16(int i) {
   static std::set<int> ignore = {};
   return ignore.find(i) != ignore.end();
 }
