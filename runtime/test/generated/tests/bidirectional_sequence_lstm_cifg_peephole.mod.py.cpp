9870732cced3c1dacc1fbcd50bec2961f7b828f8
Lev Proleev <levp@google.com>
2019-02-05 11:56:57 +0000

    Regenerate tests
    Run script to regenerate the cts tests.
    
    Test: NeuralNetworksTest_static
    Change-Id: I6d00e866ca010ba0efff8d8aaa9a14903457ec36

--- runtime/test/generated/tests/bidirectional_sequence_lstm_cifg_peephole.mod.py.cpp
+++ runtime/test/generated/tests/bidirectional_sequence_lstm_cifg_peephole.mod.py.cpp
@@ -15,11 +15,9 @@ TEST_F(GeneratedTests, bidirectional_sequence_lstm_cifg_peephole_blackbox) {
             bidirectional_sequence_lstm_cifg_peephole::get_examples_blackbox());
 }
 
-#if 0
-TEST_F(DynamicOutputShapeTests, bidirectional_sequence_lstm_cifg_peephole_blackbox_dynamic_output_shape) {
+TEST_F(DynamicOutputShapeTest, bidirectional_sequence_lstm_cifg_peephole_blackbox_dynamic_output_shape) {
     execute(bidirectional_sequence_lstm_cifg_peephole::CreateModel_dynamic_output_shape,
             bidirectional_sequence_lstm_cifg_peephole::is_ignored_dynamic_output_shape,
             bidirectional_sequence_lstm_cifg_peephole::get_examples_blackbox_dynamic_output_shape());
 }
 
-#endif
