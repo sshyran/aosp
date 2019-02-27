2a18f4a55dcc20f747aba13f1c4f24d43d198234
Xusong Wang <xusongw@google.com>
2019-02-27 14:30:46 -0800

    Apply activation in SUB.
    Bug: 113627054
    Test: NeuralNetworksTest_static
    Change-Id: I40d96d877151395e62e5b60d077635c5ca3ecce2
    Merged-In: I40d96d877151395e62e5b60d077635c5ca3ecce2
    (cherry picked from commit 024918355f9de76357e6b21b2f2e5549de5533e4)

--- nn/runtime/test/specs/V1_2/sub_v1_2_broadcast.mod.py
+++ nn/runtime/test/specs/V1_2/sub_v1_2_broadcast.mod.py
@@ -14,12 +14,34 @@
 # limitations under the License.
 #
 
+# FLOAT32 and FLOAT16
+input0 = Input("input0", "TENSOR_FLOAT32", "{1, 2}")
+input1 = Input("input1", "TENSOR_FLOAT32", "{2, 2}")
+activation = Int32Scalar("act", 0)
+output0 = Output("output0", "TENSOR_FLOAT32", "{2, 2}")
+
+model = Model().Operation("SUB", input0, input1, activation).To(output0)
+
+input0_values = [10, 20]
+input1_values = [0.1, 0.2,
+                 0.3, 0.4]
+output_values = [9.9, 19.8,
+                 9.7, 19.6]
+
+Example({
+    input0: input0_values,
+    input1: input1_values,
+    output0: output_values,
+}).AddVariations("float16").AddAllActivations(output0, activation)
+
+
+# QUANT8_ASYMM
 input0 = Input("input0", "TENSOR_QUANT8_ASYMM", "{1, 2}, 1.0, 0")
 input1 = Input("input1", "TENSOR_QUANT8_ASYMM", "{2, 2}, 1.0, 0")
 activation = 0
 output0 = Output("output0", "TENSOR_QUANT8_ASYMM", "{2, 2}, 1.0, 0")
 
-model = Model().Operation("SUB", input0, input1, activation).To(output0)
+model = Model("quant8").Operation("SUB", input0, input1, activation).To(output0)
 
 input0_values = [100, 200]
 input1_values = [1, 2,
