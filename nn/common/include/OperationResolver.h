99ff70021c7c4ab903cdbc2f96f4f6fc69739e41
Xusong Wang <xusongw@google.com>
2019-03-06 13:10:14 -0800

    Support zero-sized tensor and check for omitted input in CpuExecutor.
    Zero-sized tensors are only supported internally to the driver, or
    reported in output shapes. It is illegal to pre-specify a zero-sized
    tensor as model input or output.
    
    To summarize the meanings of dimension = 0:
    - Dimension = 0 for model input: dynamic input and must be fully-specified
    at execution time
    - Dimension = 0 for internal operand / model output: unknown, to be
    deduced from execution
    - Dimension = 0 from getOutputOperandDimensions:
      * If NO_ERROR, it is a zero-sized output
      * If OUTPUT_INSUFFICIENT_SIZE, it is unknown
    
    Add two additional fields in OperationRegistration:
    - allowOmittedOperand, if false, CpuExecutor will enforce inputs/outputs
    are not null
    - allowZeroSizedInput, if false, CpuExecutor will enforce every dimension
    of inputs not being 0
    
    The current implementation is assuming that none of the operations with
    the old switch statement path support zero-sized input. Only the
    operations with OperationResolver can support zero-sized input tensor.
    
    All the operations with OperationResolver are reporting false for
    allowZeroSizedInput. Will enable this on a small subset of operations with
    separate CLs.
    
    Bug: 126737477
    Test: NeuralNetworksTest_static
    Change-Id: Ia94d67e4c8c6a49b543d29ebb3b31d509ece0970
    Merged-In: Ia94d67e4c8c6a49b543d29ebb3b31d509ece0970
    (cherry picked from commit 5750766c01c880cd0a773738520b6942f1788ea3)

--- nn/common/include/OperationResolver.h
+++ nn/common/include/OperationResolver.h
@@ -39,6 +39,24 @@ struct OperationRegistration {
     // Executes the operation, reading from context->getInputBuffer(...)
     // and writing to context->getOutputBuffer(...).
     std::function<bool(IOperationExecutionContext*)> execute;
+
+    struct Flag {
+        // Whether the operation allows at least one operand to be omitted.
+        bool allowOmittedOperand = false;
+        // Whether the operation allows at least one input operand to be a zero-sized tensor.
+        bool allowZeroSizedInput = false;
+    } flags;
+
+    OperationRegistration(OperationType type, const char* name,
+                          std::function<bool(const IOperationValidationContext*)> validate,
+                          std::function<bool(IOperationExecutionContext*)> prepare,
+                          std::function<bool(IOperationExecutionContext*)> execute, Flag flags)
+        : type(type),
+          name(name),
+          validate(validate),
+          prepare(prepare),
+          execute(execute),
+          flags(flags) {}
 };
 
 // A registry of operation implementations.
@@ -82,32 +100,37 @@ class BuiltinOperationResolver : public IOperationResolver {
 // OperationResolver.
 //
 // Usage:
-//   namespace android {
-//   namespace nn {
-//   namespace gather {
-//   ...
-//   }  // namespace gather
+// (check OperationRegistration::Flag for available fields and default values.)
 //
-//   NN_REGISTER_OPERATION(GATHER, gather::kOperationName, gather::validate,
-//                         gather::prepare, gather::execute);
-//   }  // namespace nn
-//   }  // namespace android
+// - With default flags.
+//   NN_REGISTER_OPERATION(FOO_OP, foo_op::kOperationName, foo_op::validate,
+//                         foo_op::prepare, foo_op::execute);
+//
+// - With a customized flag.
+//   NN_REGISTER_OPERATION(FOO_OP, foo_op::kOperationName, foo_op::validate,
+//                         foo_op::prepare, foo_op::execute, .allowZeroSizedInput = true);
+//
+// - With multiple customized flags.
+//   NN_REGISTER_OPERATION(FOO_OP, foo_op::kOperationName, foo_op::validate,
+//                         foo_op::prepare, foo_op::execute, .allowOmittedOperand = true,
+//                         .allowZeroSizedInput = true);
 //
 #ifdef NN_INCLUDE_CPU_IMPLEMENTATION
-#define NN_REGISTER_OPERATION(identifier, operationName, validate, prepare, execute)           \
-    const OperationRegistration* register_##identifier() {                                     \
-        static OperationRegistration registration = {OperationType::identifier, operationName, \
-                                                     validate, prepare, execute};              \
-        return &registration;                                                                  \
+#define NN_REGISTER_OPERATION(identifier, operationName, validate, prepare, execute, ...)     \
+    const OperationRegistration* register_##identifier() {                                    \
+        static OperationRegistration registration(OperationType::identifier, operationName,   \
+                                                  validate, prepare, execute, {__VA_ARGS__}); \
+        return &registration;                                                                 \
     }
 #else
 // This version ignores CPU execution logic (prepare and execute).
 // The compiler is supposed to omit that code so that only validation logic
 // makes it into libneuralnetworks_utils.
-#define NN_REGISTER_OPERATION(identifier, operationName, validate, unused_prepare, unused_execute) \
+#define NN_REGISTER_OPERATION(identifier, operationName, validate, unused_prepare, unused_execute, \
+                              ...)                                                                 \
     const OperationRegistration* register_##identifier() {                                         \
-        static OperationRegistration registration = {OperationType::identifier, operationName,     \
-                                                     validate, nullptr, nullptr};                  \
+        static OperationRegistration registration(OperationType::identifier, operationName,        \
+                                                  validate, nullptr, nullptr, {__VA_ARGS__});      \
         return &registration;                                                                      \
     }
 #endif
