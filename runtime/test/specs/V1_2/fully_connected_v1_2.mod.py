b60491e2fed62e28ec36e581fedc057cf2d35805
Xusong Wang <xusongw@google.com>
2019-03-06 16:47:17 -0800

    Support zero batch in FULLY_CONNECTED.
    Also switch to OperationResolver.
    
    Bug: 126737477
    Test: NeuralNetworksTest_static
    Change-Id: I9f784f20e13a7cf65b5cc9af8f728c55909d2b58

--- runtime/test/specs/V1_2/fully_connected_v1_2.mod.py
+++ runtime/test/specs/V1_2/fully_connected_v1_2.mod.py
@@ -14,6 +14,7 @@
 # limitations under the License.
 #
 
+# TEST 1: FULLY_CONNECTED
 model = Model()
 in0 = Input("op1", "TENSOR_FLOAT32", "{3, 1}")
 weights = Parameter("op2", "TENSOR_FLOAT32", "{1, 1}", [2])
@@ -37,3 +38,47 @@ output0 = {out0: # output 0
 
 # Instantiate an example
 Example((input0, output0)).AddVariations("relaxed", "float16", quant8_mult_gt_1)
+
+
+# TEST 2: FULLY_CONNECTED, zero-sized input
+
+# Use BOX_WITH_NMS_LIMIT op to generate a zero-sized internal tensor for box cooridnates.
+p1 = Parameter("scores", "TENSOR_FLOAT32", "{1, 2}", [0.90, 0.10]) # scores
+p2 = Parameter("roi", "TENSOR_FLOAT32", "{1, 8}", [1, 1, 10, 10, 0, 0, 10, 10]) # roi
+o1 = Output("scoresOut", "TENSOR_FLOAT32", "{0}") # scores out
+o2 = Output("classesOut", "TENSOR_INT32", "{0}") # classes out
+tmp1 = Internal("roiOut", "TENSOR_FLOAT32", "{0, 4}") # roi out
+tmp2 = Internal("batchSplitOut", "TENSOR_INT32", "{0}") # batch split out
+model = Model("zero_sized").Operation("BOX_WITH_NMS_LIMIT", p1, p2, [0], 0.3, 0.4, -1).To(o1, tmp1, o2, tmp2)
+
+# Use ROI_ALIGN op to convert into zero-sized feature map.
+layout = BoolScalar("layout", False) # NHWC
+i1 = Input("in", "TENSOR_FLOAT32", "{1, 1, 1, 3}")
+zero_sized = Internal("featureMap", "TENSOR_FLOAT32", "{0, 2, 2, 3}")
+model = model.Operation("ROI_ALIGN", i1, tmp1, tmp2, 2, 2, 2.0, 2.0, 4, 4, layout).To(zero_sized)
+
+# FULLY_CONNECTED op with numBatches = 0.
+w = Parameter("weights", "TENSOR_FLOAT32", "{1, 3}", [1, 2, 3]) # weights
+b = Parameter("bias", "TENSOR_FLOAT32", "{1}", [1]) # bias
+o3 = Output("out", "TENSOR_FLOAT32", "{0, 1}") # out
+model = model.Operation("FULLY_CONNECTED", zero_sized, w, b, 0).To(o3)
+
+quant8 = DataTypeConverter().Identify({
+    p1: ("TENSOR_QUANT8_ASYMM", 0.1, 128),
+    p2: ("TENSOR_QUANT16_ASYMM", 0.125, 0),
+    o1: ("TENSOR_QUANT8_ASYMM", 0.1, 128),
+    tmp1: ("TENSOR_QUANT16_ASYMM", 0.125, 0),
+    i1: ("TENSOR_QUANT8_ASYMM", 0.1, 128),
+    zero_sized: ("TENSOR_QUANT8_ASYMM", 0.1, 128),
+    w: ("TENSOR_QUANT8_ASYMM", 0.1, 128),
+    b: ("TENSOR_INT32", 0.01, 0),
+    o3: ("TENSOR_QUANT8_ASYMM", 0.1, 128)
+})
+
+# Create test case with dummy values.
+Example({
+    i1: [1, 2, 3],
+    o1: [0],
+    o2: [0],
+    o3: [0],
+}).AddNchw(i1, zero_sized, layout).AddVariations("relaxed", quant8, "float16")
