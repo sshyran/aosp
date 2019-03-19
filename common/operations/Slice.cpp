7ae905ce8536515ed56f6622d88e8122d10311f5
Xusong Wang <xusongw@google.com>
2019-03-19 12:55:54 -0700

    Support zero-sized input in SLICE.
    Also switch to OperationResolver.
    
    Bug: 126737477
    Test: NeuralNetworksTest_static
    Change-Id: I4f2e5e1df3766c2846e4d1ff033280396b3f19c8
    Merged-In: I4f2e5e1df3766c2846e4d1ff033280396b3f19c8
    (cherry picked from commit 5ca7f44814a1535cf576d76cbc403b196922c13a)

--- common/operations/Slice.cpp
+++ common/operations/Slice.cpp
@@ -13,16 +13,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "Slice.h"
-
+#include "CpuOperationUtils.h"
 #include "IndexedShapeWrapper.h"
-#include "OperationsUtils.h"
+#include "OperationResolver.h"
 
 #include <vector>
 
 namespace android {
 namespace nn {
 namespace slice {
+
+constexpr char kOperationName[] = "SLICE";
+
+constexpr uint32_t kNumInputs = 3;
+constexpr uint32_t kInputTensor = 0;
+constexpr uint32_t kBeginTensor = 1;
+constexpr uint32_t kSizeTensor = 2;
+
+constexpr uint32_t kNumOutputs = 1;
+constexpr uint32_t kOutputTensor = 0;
 
 namespace {
 
@@ -66,68 +75,102 @@ bool evalGeneric(const T* inputData, const Shape& inputShape, const int32_t* beg
 
 }  // namespace
 
-bool prepare(const Shape& inputShape, const void* untypedBeginData, const Shape& beginShape,
-             const void* untypedSizeData, const Shape& sizeShape, Shape* outputShape) {
+bool validate(const IOperationValidationContext* context) {
+    NN_RET_CHECK_EQ(context->getNumInputs(), kNumInputs);
+    NN_RET_CHECK_EQ(context->getNumOutputs(), kNumOutputs);
+
+    const OperandType inputType = context->getInputType(kInputTensor);
+    NN_RET_CHECK(
+            inputType == OperandType::TENSOR_FLOAT16 || inputType == OperandType::TENSOR_FLOAT32 ||
+            inputType == OperandType::TENSOR_INT32 || inputType == OperandType::TENSOR_QUANT8_ASYMM)
+            << "Unsupported tensor type for operation " << kOperationName;
+    NN_RET_CHECK(validateHalVersion(context, HalVersion::V1_2));
+    return validateInputTypes(context,
+                              {inputType, OperandType::TENSOR_INT32, OperandType::TENSOR_INT32}) &&
+           validateOutputTypes(context, {inputType});
+}
+
+bool prepare(IOperationExecutionContext* context) {
+    const Shape& inputShape = context->getInputShape(kInputTensor);
     const int32_t n_dims = getNumberOfDimensions(inputShape);
     NN_RET_CHECK(n_dims > 0);
 
+    const Shape& beginShape = context->getInputShape(kBeginTensor);
     NN_RET_CHECK_EQ(getNumberOfDimensions(beginShape), 1);
     NN_RET_CHECK_EQ(getSizeOfDimension(beginShape, 0), n_dims);
 
+    const Shape& sizeShape = context->getInputShape(kSizeTensor);
     NN_RET_CHECK_EQ(getNumberOfDimensions(sizeShape), 1);
     NN_RET_CHECK_EQ(getSizeOfDimension(sizeShape, 0), n_dims);
 
-    const int32_t* beginData = reinterpret_cast<const int32_t*>(untypedBeginData);
-    const int32_t* sizeData = reinterpret_cast<const int32_t*>(untypedSizeData);
+    const int32_t* beginData = context->getInputBuffer<int32_t>(kBeginTensor);
+    const int32_t* sizeData = context->getInputBuffer<int32_t>(kSizeTensor);
 
-    outputShape->dimensions.resize(n_dims);
+    Shape outputShape = context->getOutputShape(kOutputTensor);
+    outputShape.dimensions.resize(n_dims);
     for (int i = 0; i < n_dims; ++i) {
         const int32_t sliceBegin = beginData[i];
         int32_t sliceSize = sizeData[i];
-        NN_RET_CHECK_LT(beginData[i], getSizeOfDimension(inputShape, i));
-        NN_RET_CHECK(sliceSize > 0 || sliceSize == -1);
         if (sliceSize == -1) {
             sliceSize = getSizeOfDimension(inputShape, i) - sliceBegin;
         }
+        NN_RET_CHECK_LE(beginData[i], getSizeOfDimension(inputShape, i));
+        NN_RET_CHECK_GE(sliceSize, 0);
         NN_RET_CHECK_LE(sliceBegin + sliceSize, getSizeOfDimension(inputShape, i));
-        outputShape->dimensions[i] = sliceSize;
+        outputShape.dimensions[i] = sliceSize;
     }
-    return true;
+    return context->setOutputShape(kOutputTensor, outputShape);
 }
 
-bool eval(const void* inputData, const Shape& inputShape, const void* untypedBeginData,
-          const Shape& beginShape, const void* untypedSizeData, const Shape& sizeShape,
-          void* outputData, const Shape& outputShape) {
-    const int32_t* beginData = reinterpret_cast<const int32_t*>(untypedBeginData);
-    const int32_t* sizeData = reinterpret_cast<const int32_t*>(untypedSizeData);
-    switch (inputShape.type) {
-        case OperandType::TENSOR_FLOAT16: {
-            return evalGeneric(reinterpret_cast<const _Float16*>(inputData), inputShape, beginData,
-                               beginShape, sizeData, sizeShape,
-                               reinterpret_cast<_Float16*>(outputData), outputShape);
-        } break;
-        case OperandType::TENSOR_FLOAT32: {
-            return evalGeneric(reinterpret_cast<const float*>(inputData), inputShape, beginData,
-                               beginShape, sizeData, sizeShape,
-                               reinterpret_cast<float*>(outputData), outputShape);
-        } break;
-        case OperandType::TENSOR_INT32: {
-            return evalGeneric(reinterpret_cast<const int32_t*>(inputData), inputShape, beginData,
-                               beginShape, sizeData, sizeShape,
-                               reinterpret_cast<int32_t*>(outputData), outputShape);
-        } break;
-        case OperandType::TENSOR_QUANT8_ASYMM: {
-            return evalGeneric(reinterpret_cast<const uint8_t*>(inputData), inputShape, beginData,
-                               beginShape, sizeData, sizeShape,
-                               reinterpret_cast<uint8_t*>(outputData), outputShape);
-        } break;
-        default: {
-            LOG(ERROR) << "Unsupported data type: " << toString(inputShape.type);
-            return false;
-        }
+bool execute(IOperationExecutionContext* context) {
+    // Bypass execution in the case of zero-sized input.
+    if (getNumberOfElements(context->getOutputShape(kOutputTensor)) == 0) return true;
+    switch (context->getInputType(kInputTensor)) {
+        case OperandType::TENSOR_FLOAT16:
+            return evalGeneric(context->getInputBuffer<_Float16>(kInputTensor),
+                               context->getInputShape(kInputTensor),
+                               context->getInputBuffer<int32_t>(kBeginTensor),
+                               context->getInputShape(kBeginTensor),
+                               context->getInputBuffer<int32_t>(kSizeTensor),
+                               context->getInputShape(kSizeTensor),
+                               context->getOutputBuffer<_Float16>(kOutputTensor),
+                               context->getOutputShape(kOutputTensor));
+        case OperandType::TENSOR_FLOAT32:
+            return evalGeneric(context->getInputBuffer<float>(kInputTensor),
+                               context->getInputShape(kInputTensor),
+                               context->getInputBuffer<int32_t>(kBeginTensor),
+                               context->getInputShape(kBeginTensor),
+                               context->getInputBuffer<int32_t>(kSizeTensor),
+                               context->getInputShape(kSizeTensor),
+                               context->getOutputBuffer<float>(kOutputTensor),
+                               context->getOutputShape(kOutputTensor));
+        case OperandType::TENSOR_INT32:
+            return evalGeneric(context->getInputBuffer<int32_t>(kInputTensor),
+                               context->getInputShape(kInputTensor),
+                               context->getInputBuffer<int32_t>(kBeginTensor),
+                               context->getInputShape(kBeginTensor),
+                               context->getInputBuffer<int32_t>(kSizeTensor),
+                               context->getInputShape(kSizeTensor),
+                               context->getOutputBuffer<int32_t>(kOutputTensor),
+                               context->getOutputShape(kOutputTensor));
+        case OperandType::TENSOR_QUANT8_ASYMM:
+            return evalGeneric(context->getInputBuffer<uint8_t>(kInputTensor),
+                               context->getInputShape(kInputTensor),
+                               context->getInputBuffer<int32_t>(kBeginTensor),
+                               context->getInputShape(kBeginTensor),
+                               context->getInputBuffer<int32_t>(kSizeTensor),
+                               context->getInputShape(kSizeTensor),
+                               context->getOutputBuffer<uint8_t>(kOutputTensor),
+                               context->getOutputShape(kOutputTensor));
+        default:
+            NN_RET_CHECK_FAIL() << "Unsupported tensor type for operation " << kOperationName;
     }
 }
 
 }  // namespace slice
+
+NN_REGISTER_OPERATION(SLICE, slice::kOperationName, slice::validate, slice::prepare, slice::execute,
+                      .allowZeroSizedInput = true);
+
 }  // namespace nn
 }  // namespace android
