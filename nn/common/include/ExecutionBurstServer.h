28ec296adae4ec7b385c050b044d577f48d3e2ae
Michael Butler <butlermichael@google.com>
2019-03-21 12:17:27 -0700

    Cache ExecutionBurstServer memory resources
    This change enables caching of memory resources. Prior to this CL, a
    hidl_memory object in an execution burst had to be mapped and unmapped
    on each execution, taking ~10us of time. With this change, a driver
    could choose to map the hidl_memory when it is first retrieved, and
    unmap when the burst is freed or when IContext::freeMemory is called.
    
    Bug: 126244806
    Test: mma
    Test: atest NeuralNetworksTest_static
    Test: atest VtsHalNeuralnetworksV1_0TargetTest (with sample-all)
    Test: atest VtsHalNeuralnetworksV1_1TargetTest (with sample-all)
    Test: atest VtsHalNeuralnetworksV1_2TargetTest (with sample-all)
    Change-Id: I92d7632f2e1e2f5e76c63249d4c615c9af9d1acb
    Merged-In: I92d7632f2e1e2f5e76c63249d4c615c9af9d1acb
    (cherry picked from commit dfc94136aff39efdfb81436cee7e73c3f6834785)

--- nn/common/include/ExecutionBurstServer.h
+++ nn/common/include/ExecutionBurstServer.h
@@ -22,6 +22,7 @@
 #include <hidl/MQDescriptor.h>
 #include <atomic>
 #include <future>
+#include <memory>
 #include <vector>
 #include "HalInterfaces.h"
 
@@ -43,28 +44,96 @@ using FmqResultDescriptor = MQDescriptorSync<FmqResultDatum>;
 class ExecutionBurstServer : public IBurstContext {
     DISALLOW_IMPLICIT_CONSTRUCTORS(ExecutionBurstServer);
 
+   public:
     /**
-     * BurstMemoryCache is responsible for managing the local memory cache of
-     * the burst object. If the ExecutionBurstServer requests a memory key that
-     * is unrecognized, the BurstMemoryCache object will retrieve the memory
-     * from the client, transparent from the ExecutionBurstServer object.
+     * IBurstExecutorWithCache is a callback object passed to
+     * ExecutionBurstServer's factory function that is used to perform an
+     * execution. Because some memory resources are needed across multiple
+     * executions, this object also contains a local cache that can directly be
+     * used in the execution.
+     *
+     * ExecutionBurstServer will never access its IBurstExecutorWithCache object
+     * with concurrent calls.
      */
-    class BurstMemoryCache {
-        DISALLOW_IMPLICIT_CONSTRUCTORS(BurstMemoryCache);
+    class IBurstExecutorWithCache {
+        DISALLOW_COPY_AND_ASSIGN(IBurstExecutorWithCache);
 
        public:
-        BurstMemoryCache(const sp<IBurstCallback>& callback);
+        IBurstExecutorWithCache() = default;
+        virtual ~IBurstExecutorWithCache() = default;
 
-        hidl_vec<hidl_memory> getMemories(const std::vector<int32_t>& slots);
-        void freeMemory(int32_t slot);
+        /**
+         * Checks if a cache entry specified by a slot is present in the cache.
+         *
+         * @param slot Identifier of the cache entry.
+         * @return 'true' if the cache entry is present in the cache, 'false'
+         *     otherwise.
+         */
+        virtual bool isCacheEntryPresent(int32_t slot) const = 0;
 
-       private:
-        std::mutex mMutex;
-        const sp<IBurstCallback> mCallback;
-        std::vector<hidl_memory> mMemoryCache;
+        /**
+         * Adds an entry specified by a slot to the cache.
+         *
+         * The caller of this function must ensure that the cache entry that is
+         * being added is not already present in the cache. This can be checked
+         * via isCacheEntryPresent.
+         *
+         * @param memory Memory resource to be cached.
+         * @param slot Slot identifier corresponding to the memory resource.
+         */
+        virtual void addCacheEntry(const hidl_memory& memory, int32_t slot) = 0;
+
+        /**
+         * Removes an entry specified by a slot from the cache.
+         *
+         * If the cache entry corresponding to the slot number does not exist,
+         * the call does nothing.
+         *
+         * @param slot Slot identifier corresponding to the memory resource.
+         */
+        virtual void removeCacheEntry(int32_t slot) = 0;
+
+        /**
+         * Perform an execution.
+         *
+         * @param request Request object with inputs and outputs specified.
+         *     Request::pools is empty, and DataLocation::poolIndex instead
+         *     refers to the 'slots' argument as if it were Request::pools.
+         * @param slots Slots corresponding to the cached memory entries to be
+         *     used.
+         * @param measure Whether timing information is requested for the
+         *     execution.
+         * @return Result of the execution, including the status of the
+         *     execution, dynamic output shapes, and any timing information.
+         */
+        virtual std::tuple<ErrorStatus, hidl_vec<OutputShape>, Timing> execute(
+                const Request& request, const std::vector<int32_t>& slots,
+                MeasureTiming measure) = 0;
     };
 
-   public:
+    /**
+     * Create automated context to manage FMQ-based executions.
+     *
+     * This function is intended to be used by a service to automatically:
+     * 1) Receive data from a provided FMQ
+     * 2) Execute a model with the given information
+     * 3) Send the result to the created FMQ
+     *
+     * @param callback Callback used to retrieve memories corresponding to
+     *     unrecognized slots.
+     * @param requestChannel Input FMQ channel through which the client passes the
+     *     request to the service.
+     * @param resultChannel Output FMQ channel from which the client can retrieve
+     *     the result of the execution.
+     * @param executorWithCache Object which maintains a local cache of the
+     *     memory pools and executes using the cached memory pools.
+     * @result IBurstContext Handle to the burst context.
+     */
+    static sp<ExecutionBurstServer> create(
+            const sp<IBurstCallback>& callback, const FmqRequestDescriptor& requestChannel,
+            const FmqResultDescriptor& resultChannel,
+            std::shared_ptr<IBurstExecutorWithCache> executorWithCache);
+
     /**
      * Create automated context to manage FMQ-based executions.
      *
@@ -80,7 +149,8 @@ class ExecutionBurstServer : public IBurstContext {
      * @param resultChannel Output FMQ channel from which the client can retrieve
      *     the result of the execution.
      * @param preparedModel PreparedModel that the burst object was created from.
-     *     This will be used to synchronously perform the execution.
+     *     IPreparedModel::executeSynchronously will be used to perform the
+     *     execution.
      * @result IBurstContext Handle to the burst context.
      */
     static sp<ExecutionBurstServer> create(const sp<IBurstCallback>& callback,
@@ -91,7 +161,7 @@ class ExecutionBurstServer : public IBurstContext {
     ExecutionBurstServer(const sp<IBurstCallback>& callback,
                          std::unique_ptr<FmqRequestChannel> requestChannel,
                          std::unique_ptr<FmqResultChannel> resultChannel,
-                         IPreparedModel* preparedModel);
+                         std::shared_ptr<IBurstExecutorWithCache> cachedExecutor);
     ~ExecutionBurstServer();
 
     Return<void> freeMemory(int32_t slot) override;
@@ -102,15 +172,32 @@ class ExecutionBurstServer : public IBurstContext {
     std::vector<FmqResultDatum> serialize(ErrorStatus errorStatus,
                                           const std::vector<OutputShape>& outputShapes,
                                           Timing timing);
-    std::pair<Request, MeasureTiming> deserialize(const std::vector<FmqRequestDatum>& data);
+
+    // Deserializes the request FMQ data. The three resulting fields are the
+    // Request object (where Request::pools is empty), slot identifiers (which
+    // are stand-ins for Request::pools), and whether timing information must
+    // be collected for the run.
+    std::tuple<Request, std::vector<int32_t>, MeasureTiming> deserialize(
+            const std::vector<FmqRequestDatum>& data);
+
+    // Ensures all cache entries contained in mExecutorWithCache are present in
+    // the cache. If they are not present, they are retrieved (via
+    // IBurstCallback::getMemories) and added to mExecutorWithCache.
+    //
+    // This method is locked via mMutex when it is called.
+    void ensureCacheEntriesArePresentLocked(const std::vector<int32_t>& slots);
+
+    // Work loop that will continue processing execution requests until the
+    // ExecutionBurstServer object is freed.
     void task();
 
-    BurstMemoryCache mMemoryCache;
+    std::mutex mMutex;
     std::atomic<bool> mTeardown{false};
     std::future<void> mWorker;
+    const sp<IBurstCallback> mCallback;
     const std::unique_ptr<FmqRequestChannel> mFmqRequestChannel;
     const std::unique_ptr<FmqResultChannel> mFmqResultChannel;
-    IPreparedModel* mPreparedModel;
+    const std::shared_ptr<IBurstExecutorWithCache> mExecutorWithCache;
     const bool mBlocking;
 };
 
