11e529c9b2448f7d6c37730b3994463861257c79
Xusong Wang <xusongw@google.com>
2019-04-15 10:47:41 -0700

    Reorganize OperationManager in random graph generator.
    * Mark the operation signatures with supported data types, ranks, and
      hal version. Defines the operation filter to select signatures by
      opcode, data type, rank, and hal version.
    
    * Move operation signature defs into separate files as it becomes hard
      to manage.
    
    * Provide a way to automatically add the operation signature to
      OperationManager when it is defined.
    
    * Separate the tests:
      - Separate single operation tests based on opcode and hal version.
        Each opcode of each version will run 100 iterations.
      - Separate small and large graph tests with data type and rank.
      - Operations that are currently not defined are listed as TODO.
    
    Bug: 129569810
    Test: NeuralNetworksTest_static_fuzzing
    Change-Id: Ic38ec2bed72d3f7f6ed21e1aa94e101d238e3df6
    Merged-In: Ic38ec2bed72d3f7f6ed21e1aa94e101d238e3df6
    (cherry picked from commit fabbbfa5aa088297da7ec3ca0f80bed1512d0bbc)

--- nn/runtime/test/fuzzing/RandomGraphGeneratorUtils.h
+++ nn/runtime/test/fuzzing/RandomGraphGeneratorUtils.h
@@ -17,17 +17,16 @@
 #ifndef ANDROID_FRAMEWORK_ML_NN_RUNTIME_TEST_FUZZING_RANDOM_GRAPH_GENERATOR_UTILS_H
 #define ANDROID_FRAMEWORK_ML_NN_RUNTIME_TEST_FUZZING_RANDOM_GRAPH_GENERATOR_UTILS_H
 
-#include "RandomGraphGenerator.h"
-#include "RandomVariable.h"
-
-#include "TestNeuralNetworksWrapper.h"
-
 #include <chrono>
 #include <fstream>
 #include <memory>
 #include <random>
 #include <sstream>
 #include <string>
+
+#include "RandomGraphGenerator.h"
+#include "RandomVariable.h"
+#include "TestNeuralNetworksWrapper.h"
 
 namespace android {
 namespace nn {
@@ -81,7 +80,10 @@ class LoggerStream {
     LoggerStream(bool abortAfterLog) : mAbortAfterLog(abortAfterLog) {}
     ~LoggerStream() {
         Logger::get()->log(ss.str() + '\n');
-        if (mAbortAfterLog) abort();
+        if (mAbortAfterLog) {
+            std::cout << ss.str() << std::endl;
+            abort();
+        }
     }
 
     template <typename T>
@@ -167,10 +169,89 @@ static const char* kOperationNames[] = {
         "SPACE_TO_DEPTH",
         "SVDF",
         "TANH",
+        "BATCH_TO_SPACE_ND",
+        "DIV",
+        "MEAN",
+        "PAD",
+        "SPACE_TO_BATCH_ND",
+        "SQUEEZE",
+        "STRIDED_SLICE",
+        "SUB",
+        "TRANSPOSE",
+        "ABS",
+        "ARGMAX",
+        "ARGMIN",
+        "AXIS_ALIGNED_BBOX_TRANSFORM",
+        "BIDIRECTIONAL_SEQUENCE_LSTM",
+        "BIDIRECTIONAL_SEQUENCE_RNN",
+        "BOX_WITH_NMS_LIMIT",
+        "CAST",
+        "CHANNEL_SHUFFLE",
+        "DETECTION_POSTPROCESSING",
+        "EQUAL",
+        "EXP",
+        "EXPAND_DIMS",
+        "GATHER",
+        "GENERATE_PROPOSALS",
+        "GREATER",
+        "GREATER_EQUAL",
+        "GROUPED_CONV_2D",
+        "HEATMAP_MAX_KEYPOINT",
+        "INSTANCE_NORMALIZATION",
+        "LESS",
+        "LESS_EQUAL",
+        "LOG",
+        "LOGICAL_AND",
+        "LOGICAL_NOT",
+        "LOGICAL_OR",
+        "LOG_SOFTMAX",
+        "MAXIMUM",
+        "MINIMUM",
+        "NEG",
+        "NOT_EQUAL",
+        "PAD_V2",
+        "POW",
+        "PRELU",
+        "QUANTIZE",
+        "QUANTIZED_16BIT_LSTM",
+        "RANDOM_MULTINOMIAL",
+        "REDUCE_ALL",
+        "REDUCE_ANY",
+        "REDUCE_MAX",
+        "REDUCE_MIN",
+        "REDUCE_PROD",
+        "REDUCE_SUM",
+        "ROI_ALIGN",
+        "ROI_POOLING",
+        "RSQRT",
+        "SELECT",
+        "SIN",
+        "SLICE",
+        "SPLIT",
+        "SQRT",
+        "TILE",
+        "TOPK_V2",
+        "TRANSPOSE_CONV_2D",
+        "UNIDIRECTIONAL_SEQUENCE_LSTM",
+        "UNIDIRECTIONAL_SEQUENCE_RNN",
+        "RESIZE_NEAREST_NEIGHBOR",
 };
 
 static const char* kTypeNames[] = {
-        "FLOAT32", "INT32", "UINT32", "TENSOR_FLOAT32", "TENSOR_INT32", "TENSOR_QUANT8_ASYMM",
+        "FLOAT32",
+        "INT32",
+        "UINT32",
+        "TENSOR_FLOAT32",
+        "TENSOR_INT32",
+        "TENSOR_QUANT8_ASYMM",
+        "BOOL",
+        "TENSOR_QUANT16_SYMM",
+        "TENSOR_FLOAT16",
+        "TENSOR_BOOL8",
+        "FLOAT16",
+        "TENSOR_QUANT8_SYMM_PER_CHANNEL",
+        "TENSOR_QUANT16_ASYMM",
+        "TENSOR_QUANT8_SYMM",
 };
 
 static const char* kLifeTimeNames[6] = {
@@ -185,6 +266,14 @@ static const bool kScalarDataType[]{
         false,  // ANEURALNETWORKS_TENSOR_FLOAT32
         false,  // ANEURALNETWORKS_TENSOR_INT32
         false,  // ANEURALNETWORKS_TENSOR_SYMMETRICAL_QUANT8
+        true,   // ANEURALNETWORKS_BOOL
+        false,  // ANEURALNETWORKS_TENSOR_QUANT16_SYMM
+        false,  // ANEURALNETWORKS_TENSOR_FLOAT16
+        false,  // ANEURALNETWORKS_TENSOR_BOOL8
+        true,   // ANEURALNETWORKS_FLOAT16
+        false,  // ANEURALNETWORKS_TENSOR_QUANT8_SYMM_PER_CHANNEL
+        false,  // ANEURALNETWORKS_TENSOR_QUANT16_ASYMM
+        false,  // ANEURALNETWORKS_TENSOR_QUANT8_SYMM
 };
 
 static const uint32_t kSizeOfDataType[]{
@@ -194,6 +283,14 @@ static const uint32_t kSizeOfDataType[]{
         4,  // ANEURALNETWORKS_TENSOR_FLOAT32
         4,  // ANEURALNETWORKS_TENSOR_INT32
         1,  // ANEURALNETWORKS_TENSOR_SYMMETRICAL_QUANT8
+        1,  // ANEURALNETWORKS_BOOL
+        2,  // ANEURALNETWORKS_TENSOR_QUANT16_SYMM
+        2,  // ANEURALNETWORKS_TENSOR_FLOAT16
+        1,  // ANEURALNETWORKS_TENSOR_BOOL8
+        2,  // ANEURALNETWORKS_FLOAT16
+        1,  // ANEURALNETWORKS_TENSOR_QUANT8_SYMM_PER_CHANNEL
+        2,  // ANEURALNETWORKS_TENSOR_QUANT16_ASYMM
+        1,  // ANEURALNETWORKS_TENSOR_QUANT8_SYMM
 };
 
 template <>
@@ -257,8 +354,22 @@ inline std::string toString<RandomOperand>(const RandomOperand& op) {
     return toString(op.type) + ", dimension = [" +
            joinStr(", ", op.dimensions,
                    [](const RandomVariable& var) { return std::to_string(var.getValue()); }) +
-           "]";
+           "], scale = " + toString(op.scale) + " , zero_point = " + toString(op.zeroPoint);
 }
+
+// This class is a workaround for two issues our code relies on:
+// 1. sizeof(bool) is implementation defined.
+// 2. vector<bool> does not allow direct pointer access via the data() method.
+class bool8 {
+   public:
+    bool8() : mValue() {}
+    /* implicit */ bool8(bool value) : mValue(value) {}
+    inline operator bool() const { return mValue != 0; }
+
+   private:
+    uint8_t mValue;
+};
+static_assert(sizeof(bool8) == 1, "size of bool8 must be 8 bits");
 
 // Dump the random graph to a spec file.
 class SpecWriter {
@@ -278,7 +389,15 @@ class SpecWriter {
     void dump(const T* buffer, uint32_t length) {
         for (uint32_t i = 0; i < length; i++) {
             if (i != 0) os << ", ";
-            os << buffer[i];
+            if constexpr (std::is_integral<T>::value) {
+                os << static_cast<int>(buffer[i]);
+            } else if constexpr (std::is_same<T, _Float16>::value) {
+                os << static_cast<float>(buffer[i]);
+            } else if constexpr (std::is_same<T, bool8>::value) {
+                os << (buffer[i] ? "True" : "False");
+            } else {
+                os << buffer[i];
+            }
         }
     }
 
@@ -294,16 +413,22 @@ inline bool getBernoulli(double p) {
     return dis(RandomNumberGenerator::generator);
 }
 
+#define NN_IS_FLOAT(T) std::is_floating_point_v<T> || std::is_same_v<T, _Float16>
+
+// getUniform for floating point values operates on a open interval (lower, upper).
+// This is important for generating a scale that is greater than but not equal to a lower bound.
 template <typename T>
-inline T getUniform(T, T);
-template <>
-inline float getUniform<float>(float lower, float upper) {
-    std::uniform_real_distribution<float> dis(lower, upper);
+inline std::enable_if_t<NN_IS_FLOAT(T), T> getUniform(T lower, T upper) {
+    float nextLower = std::nextafter(static_cast<float>(lower), std::numeric_limits<float>::max());
+    std::uniform_real_distribution<float> dis(nextLower, upper);
     return dis(RandomNumberGenerator::generator);
 }
-template <>
-inline int32_t getUniform<int32_t>(int32_t lower, int32_t upper) {
-    std::uniform_int_distribution<int32_t> dis(lower, upper);
+
+// getUniform for integers operates on a closed interval [lower, upper].
+// This is important that 255 should be included as a valid candidate for QUANT8_ASYMM values.
+template <typename T>
+inline std::enable_if_t<std::is_integral_v<T>, T> getUniform(T lower, T upper) {
+    std::uniform_int_distribution<T> dis(lower, upper);
     return dis(RandomNumberGenerator::generator);
 }
 
