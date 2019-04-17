0d2cda4639f6f3186709532da95f2d04c47b5d26
Xusong Wang <xusongw@google.com>
2019-04-17 15:40:28 -0700

    Define GATHER, SELECT, TOPK in operation manager.
    Bug: 129569810
    Test: NeuralNetworksTest_static_fuzzing
    Change-Id: I1b5ee1473fddc96a184c5ef23e81c6d89556c46e

--- nn/runtime/test/fuzzing/operation_signatures/Selection.cpp
+++ nn/runtime/test/fuzzing/operation_signatures/Selection.cpp
@@ -99,6 +99,99 @@ DEFINE_OPERATION_SIGNATURE(HASHTABLE_LOOKUP_V1_0){
         .constructor = hashtableLookupConstructor,
         .finalizer = hashtableLookupFinalizer};
 
+static void gatherConstructor(Type, uint32_t rank, RandomOperation* op) {
+    // Generate value for "axis" scalar.
+    int32_t axis = getUniform<int32_t>(-rank, rank - 1);
+    op->inputs[1]->setScalarValue<int32_t>(axis);
+    if (axis < 0) axis += rank;
+
+    // Set dimensions for input and indices tensor.
+    uint32_t indRank = getUniform<uint32_t>(1, 5);
+    setFreeDimensions(op->inputs[0], rank);
+    setFreeDimensions(op->inputs[2], indRank);
+
+    for (uint32_t i = 0; i < static_cast<uint32_t>(axis); i++) {
+        op->outputs[0]->dimensions.push_back(op->inputs[0]->dimensions[i]);
+    }
+    for (uint32_t i = 0; i < indRank; i++) {
+        op->outputs[0]->dimensions.push_back(op->inputs[2]->dimensions[i]);
+    }
+    for (uint32_t i = axis + 1; i < rank; i++) {
+        op->outputs[0]->dimensions.push_back(op->inputs[0]->dimensions[i]);
+    }
+    setSameQuantization(op->outputs[0], op->inputs[0]);
+}
+
+static void gatherFinalizer(RandomOperation* op) {
+    int32_t axis = op->inputs[1]->value<int32_t>();
+    if (axis < 0) axis += op->inputs[0]->dimensions.size();
+    uint32_t dimValue = op->inputs[0]->dimensions[axis].getValue();
+    uint32_t numElements = op->inputs[2]->getNumberOfElements();
+    for (uint32_t i = 0; i < numElements; i++) {
+        // The index values must be in the range of [0, dimValue).
+        op->inputs[2]->value<int32_t>(i) = getUniform<int32_t>(0, dimValue - 1);
+    }
+}
+
+DEFINE_OPERATION_SIGNATURE(GATHER_V1_2){
+        .opType = ANEURALNETWORKS_GATHER,
+        .supportedDataTypes = {Type::TENSOR_FLOAT32, Type::TENSOR_FLOAT16, Type::TENSOR_INT32,
+                               Type::TENSOR_QUANT8_ASYMM},
+        .supportedRanks = {1, 2, 3, 4, 5},
+        .version = HalVersion::V1_2,
+        .inputs = {INPUT_DEFAULT, PARAMETER_NONE(Type::INT32), PARAMETER_NONE(Type::TENSOR_INT32)},
+        .outputs = {OUTPUT_DEFAULT},
+        .constructor = gatherConstructor,
+        .finalizer = gatherFinalizer};
+
+static void selectConstructor(Type, uint32_t rank, RandomOperation* op) {
+    setFreeDimensions(op->inputs[0], rank);
+    op->inputs[1]->dimensions = op->inputs[0]->dimensions;
+    op->inputs[2]->dimensions = op->inputs[0]->dimensions;
+    op->outputs[0]->dimensions = op->inputs[0]->dimensions;
+    setSameQuantization(op->inputs[2], op->inputs[1]);
+    setSameQuantization(op->outputs[0], op->inputs[1]);
+}
+
+DEFINE_OPERATION_SIGNATURE(SELECT_V1_2){
+        .opType = ANEURALNETWORKS_SELECT,
+        .supportedDataTypes = {Type::TENSOR_FLOAT32, Type::TENSOR_FLOAT16, Type::TENSOR_INT32,
+                               Type::TENSOR_QUANT8_ASYMM},
+        .supportedRanks = {1, 2, 3, 4},
+        .version = HalVersion::V1_2,
+        .inputs = {INPUT_TYPED(Type::TENSOR_BOOL8), INPUT_DEFAULT, INPUT_DEFAULT},
+        .outputs = {OUTPUT_DEFAULT},
+        .constructor = selectConstructor};
+
+static void topKConstructor(Type, uint32_t rank, RandomOperation* op) {
+    setFreeDimensions(op->inputs[0], rank);
+    op->outputs[0]->dimensions.resize(rank);
+    op->outputs[1]->dimensions.resize(rank);
+    for (uint32_t i = 0; i < rank - 1; i++) {
+        op->outputs[0]->dimensions[i] = op->inputs[0]->dimensions[i];
+        op->outputs[1]->dimensions[i] = op->inputs[0]->dimensions[i];
+    }
+
+    // K must be in the range of [1, depth].
+    auto k = op->inputs[1]->value<RandomVariable>();
+    k.setRange(1, kInvalidValue);
+    op->inputs[0]->dimensions.back().setGreaterEqual(k);
+
+    op->outputs[0]->dimensions.back() = k;
+    op->outputs[1]->dimensions.back() = k;
+    setSameQuantization(op->outputs[0], op->inputs[0]);
+}
+
+DEFINE_OPERATION_SIGNATURE(TOPK_V2_V1_2){
+        .opType = ANEURALNETWORKS_TOPK_V2,
+        .supportedDataTypes = {Type::TENSOR_FLOAT32, Type::TENSOR_FLOAT16, Type::TENSOR_INT32,
+                               Type::TENSOR_QUANT8_ASYMM},
+        .supportedRanks = {1, 2, 3, 4},
+        .version = HalVersion::V1_2,
+        .inputs = {INPUT_DEFAULT, RANDOM_INT_FREE},
+        .outputs = {OUTPUT_DEFAULT, OUTPUT_TYPED(Type::TENSOR_INT32)},
+        .constructor = topKConstructor};
+
 }  // namespace fuzzing_test
 }  // namespace nn
 }  // namespace android
