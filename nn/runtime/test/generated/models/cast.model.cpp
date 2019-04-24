aba3da9166552d9f31dd051acb68180651f21b5d
Xusong Wang <xusongw@google.com>
2019-04-24 12:49:07 -0700

    Fix under/overflow issue in requantize and CAST.
    Also add tests. requantize is tested in MAXIMUM and MINIMUM.
    
    Bug: 131111895
    Test: NeuralNetworksTest_static
    Change-Id: I8971966610542feacf188fd729286ca8d285f92c
    Merged-In: I8971966610542feacf188fd729286ca8d285f92c
    (cherry picked from commit ef7ee07b2c6e5f02a55d54576ee29572d9a97e64)

--- nn/runtime/test/generated/models/cast.model.cpp
+++ nn/runtime/test/generated/models/cast.model.cpp
@@ -21,10 +21,10 @@ inline bool is_ignored(int i) {
 
 void CreateModel_dynamic_output_shape(Model *model) {
   OperandType type0(Type::TENSOR_FLOAT16, {2, 3});
-  OperandType type4(Type::TENSOR_FLOAT16, {0, 0});
+  OperandType type8(Type::TENSOR_FLOAT16, {0, 0});
   // Phase 1, operands
   auto input0 = model->addOperand(&type0);
-  auto output0 = model->addOperand(&type4);
+  auto output0 = model->addOperand(&type8);
   // Phase 2, operations
   model->addOperation(ANEURALNETWORKS_CAST, {input0}, {output0});
   // Phase 3, inputs and outputs
@@ -83,10 +83,10 @@ inline bool is_ignored_relaxed(int i) {
 
 void CreateModel_dynamic_output_shape_2(Model *model) {
   OperandType type0(Type::TENSOR_FLOAT16, {2, 3});
-  OperandType type5(Type::TENSOR_FLOAT32, {0, 0});
+  OperandType type9(Type::TENSOR_FLOAT32, {0, 0});
   // Phase 1, operands
   auto input0 = model->addOperand(&type0);
-  auto output01 = model->addOperand(&type5);
+  auto output01 = model->addOperand(&type9);
   // Phase 2, operations
   model->addOperation(ANEURALNETWORKS_CAST, {input0}, {output01});
   // Phase 3, inputs and outputs
@@ -103,10 +103,10 @@ inline bool is_ignored_dynamic_output_shape_2(int i) {
 
 void CreateModel_dynamic_output_shape_relaxed(Model *model) {
   OperandType type0(Type::TENSOR_FLOAT16, {2, 3});
-  OperandType type5(Type::TENSOR_FLOAT32, {0, 0});
+  OperandType type9(Type::TENSOR_FLOAT32, {0, 0});
   // Phase 1, operands
   auto input0 = model->addOperand(&type0);
-  auto output01 = model->addOperand(&type5);
+  auto output01 = model->addOperand(&type9);
   // Phase 2, operations
   model->addOperation(ANEURALNETWORKS_CAST, {input0}, {output01});
   // Phase 3, inputs and outputs
@@ -145,10 +145,10 @@ inline bool is_ignored_3(int i) {
 
 void CreateModel_dynamic_output_shape_3(Model *model) {
   OperandType type0(Type::TENSOR_FLOAT16, {2, 3});
-  OperandType type6(Type::TENSOR_INT32, {0, 0});
+  OperandType type10(Type::TENSOR_INT32, {0, 0});
   // Phase 1, operands
   auto input0 = model->addOperand(&type0);
-  auto output02 = model->addOperand(&type6);
+  auto output02 = model->addOperand(&type10);
   // Phase 2, operations
   model->addOperation(ANEURALNETWORKS_CAST, {input0}, {output02});
   // Phase 3, inputs and outputs
@@ -185,10 +185,10 @@ inline bool is_ignored_4(int i) {
 
 void CreateModel_dynamic_output_shape_4(Model *model) {
   OperandType type0(Type::TENSOR_FLOAT16, {2, 3});
-  OperandType type7(Type::TENSOR_QUANT8_ASYMM, {0, 0}, 4.0f, 100);
+  OperandType type11(Type::TENSOR_QUANT8_ASYMM, {0, 0}, 4.0f, 100);
   // Phase 1, operands
   auto input0 = model->addOperand(&type0);
-  auto output03 = model->addOperand(&type7);
+  auto output03 = model->addOperand(&type11);
   // Phase 2, operations
   model->addOperation(ANEURALNETWORKS_CAST, {input0}, {output03});
   // Phase 3, inputs and outputs
@@ -247,10 +247,10 @@ inline bool is_ignored_relaxed_2(int i) {
 
 void CreateModel_dynamic_output_shape_5(Model *model) {
   OperandType type1(Type::TENSOR_FLOAT32, {2, 3});
-  OperandType type4(Type::TENSOR_FLOAT16, {0, 0});
+  OperandType type8(Type::TENSOR_FLOAT16, {0, 0});
   // Phase 1, operands
   auto input01 = model->addOperand(&type1);
-  auto output0 = model->addOperand(&type4);
+  auto output0 = model->addOperand(&type8);
   // Phase 2, operations
   model->addOperation(ANEURALNETWORKS_CAST, {input01}, {output0});
   // Phase 3, inputs and outputs
@@ -267,10 +267,10 @@ inline bool is_ignored_dynamic_output_shape_5(int i) {
 
 void CreateModel_dynamic_output_shape_relaxed_2(Model *model) {
   OperandType type1(Type::TENSOR_FLOAT32, {2, 3});
-  OperandType type4(Type::TENSOR_FLOAT16, {0, 0});
+  OperandType type8(Type::TENSOR_FLOAT16, {0, 0});
   // Phase 1, operands
   auto input01 = model->addOperand(&type1);
-  auto output0 = model->addOperand(&type4);
+  auto output0 = model->addOperand(&type8);
   // Phase 2, operations
   model->addOperation(ANEURALNETWORKS_CAST, {input01}, {output0});
   // Phase 3, inputs and outputs
@@ -329,10 +329,10 @@ inline bool is_ignored_relaxed_3(int i) {
 
 void CreateModel_dynamic_output_shape_6(Model *model) {
   OperandType type1(Type::TENSOR_FLOAT32, {2, 3});
-  OperandType type5(Type::TENSOR_FLOAT32, {0, 0});
+  OperandType type9(Type::TENSOR_FLOAT32, {0, 0});
   // Phase 1, operands
   auto input01 = model->addOperand(&type1);
-  auto output01 = model->addOperand(&type5);
+  auto output01 = model->addOperand(&type9);
   // Phase 2, operations
   model->addOperation(ANEURALNETWORKS_CAST, {input01}, {output01});
   // Phase 3, inputs and outputs
@@ -349,10 +349,10 @@ inline bool is_ignored_dynamic_output_shape_6(int i) {
 
 void CreateModel_dynamic_output_shape_relaxed_3(Model *model) {
   OperandType type1(Type::TENSOR_FLOAT32, {2, 3});
-  OperandType type5(Type::TENSOR_FLOAT32, {0, 0});
+  OperandType type9(Type::TENSOR_FLOAT32, {0, 0});
   // Phase 1, operands
   auto input01 = model->addOperand(&type1);
-  auto output01 = model->addOperand(&type5);
+  auto output01 = model->addOperand(&type9);
   // Phase 2, operations
   model->addOperation(ANEURALNETWORKS_CAST, {input01}, {output01});
   // Phase 3, inputs and outputs
@@ -413,10 +413,10 @@ inline bool is_ignored_relaxed_4(int i) {
 
 void CreateModel_dynamic_output_shape_7(Model *model) {
   OperandType type1(Type::TENSOR_FLOAT32, {2, 3});
-  OperandType type6(Type::TENSOR_INT32, {0, 0});
+  OperandType type10(Type::TENSOR_INT32, {0, 0});
   // Phase 1, operands
   auto input01 = model->addOperand(&type1);
-  auto output02 = model->addOperand(&type6);
+  auto output02 = model->addOperand(&type10);
   // Phase 2, operations
   model->addOperation(ANEURALNETWORKS_CAST, {input01}, {output02});
   // Phase 3, inputs and outputs
@@ -433,10 +433,10 @@ inline bool is_ignored_dynamic_output_shape_7(int i) {
 
 void CreateModel_dynamic_output_shape_relaxed_4(Model *model) {
   OperandType type1(Type::TENSOR_FLOAT32, {2, 3});
-  OperandType type6(Type::TENSOR_INT32, {0, 0});
+  OperandType type10(Type::TENSOR_INT32, {0, 0});
   // Phase 1, operands
   auto input01 = model->addOperand(&type1);
-  auto output02 = model->addOperand(&type6);
+  auto output02 = model->addOperand(&type10);
   // Phase 2, operations
   model->addOperation(ANEURALNETWORKS_CAST, {input01}, {output02});
   // Phase 3, inputs and outputs
@@ -497,10 +497,10 @@ inline bool is_ignored_relaxed_5(int i) {
 
 void CreateModel_dynamic_output_shape_8(Model *model) {
   OperandType type1(Type::TENSOR_FLOAT32, {2, 3});
-  OperandType type7(Type::TENSOR_QUANT8_ASYMM, {0, 0}, 4.0f, 100);
+  OperandType type11(Type::TENSOR_QUANT8_ASYMM, {0, 0}, 4.0f, 100);
   // Phase 1, operands
   auto input01 = model->addOperand(&type1);
-  auto output03 = model->addOperand(&type7);
+  auto output03 = model->addOperand(&type11);
   // Phase 2, operations
   model->addOperation(ANEURALNETWORKS_CAST, {input01}, {output03});
   // Phase 3, inputs and outputs
@@ -517,10 +517,10 @@ inline bool is_ignored_dynamic_output_shape_8(int i) {
 
 void CreateModel_dynamic_output_shape_relaxed_5(Model *model) {
   OperandType type1(Type::TENSOR_FLOAT32, {2, 3});
-  OperandType type7(Type::TENSOR_QUANT8_ASYMM, {0, 0}, 4.0f, 100);
+  OperandType type11(Type::TENSOR_QUANT8_ASYMM, {0, 0}, 4.0f, 100);
   // Phase 1, operands
   auto input01 = model->addOperand(&type1);
-  auto output03 = model->addOperand(&type7);
+  auto output03 = model->addOperand(&type11);
   // Phase 2, operations
   model->addOperation(ANEURALNETWORKS_CAST, {input01}, {output03});
   // Phase 3, inputs and outputs
@@ -559,10 +559,10 @@ inline bool is_ignored_9(int i) {
 
 void CreateModel_dynamic_output_shape_9(Model *model) {
   OperandType type2(Type::TENSOR_INT32, {2, 3});
-  OperandType type4(Type::TENSOR_FLOAT16, {0, 0});
+  OperandType type8(Type::TENSOR_FLOAT16, {0, 0});
   // Phase 1, operands
   auto input02 = model->addOperand(&type2);
-  auto output0 = model->addOperand(&type4);
+  auto output0 = model->addOperand(&type8);
   // Phase 2, operations
   model->addOperation(ANEURALNETWORKS_CAST, {input02}, {output0});
   // Phase 3, inputs and outputs
@@ -621,10 +621,10 @@ inline bool is_ignored_relaxed_6(int i) {
 
 void CreateModel_dynamic_output_shape_10(Model *model) {
   OperandType type2(Type::TENSOR_INT32, {2, 3});
-  OperandType type5(Type::TENSOR_FLOAT32, {0, 0});
+  OperandType type9(Type::TENSOR_FLOAT32, {0, 0});
   // Phase 1, operands
   auto input02 = model->addOperand(&type2);
-  auto output01 = model->addOperand(&type5);
+  auto output01 = model->addOperand(&type9);
   // Phase 2, operations
   model->addOperation(ANEURALNETWORKS_CAST, {input02}, {output01});
   // Phase 3, inputs and outputs
@@ -641,10 +641,10 @@ inline bool is_ignored_dynamic_output_shape_10(int i) {
 
 void CreateModel_dynamic_output_shape_relaxed_6(Model *model) {
   OperandType type2(Type::TENSOR_INT32, {2, 3});
-  OperandType type5(Type::TENSOR_FLOAT32, {0, 0});
+  OperandType type9(Type::TENSOR_FLOAT32, {0, 0});
   // Phase 1, operands
   auto input02 = model->addOperand(&type2);
-  auto output01 = model->addOperand(&type5);
+  auto output01 = model->addOperand(&type9);
   // Phase 2, operations
   model->addOperation(ANEURALNETWORKS_CAST, {input02}, {output01});
   // Phase 3, inputs and outputs
@@ -681,11 +681,11 @@ inline bool is_ignored_11(int i) {
 }
 
 void CreateModel_dynamic_output_shape_11(Model *model) {
+  OperandType type10(Type::TENSOR_INT32, {0, 0});
   OperandType type2(Type::TENSOR_INT32, {2, 3});
-  OperandType type6(Type::TENSOR_INT32, {0, 0});
   // Phase 1, operands
   auto input02 = model->addOperand(&type2);
-  auto output02 = model->addOperand(&type6);
+  auto output02 = model->addOperand(&type10);
   // Phase 2, operations
   model->addOperation(ANEURALNETWORKS_CAST, {input02}, {output02});
   // Phase 3, inputs and outputs
@@ -721,11 +721,11 @@ inline bool is_ignored_12(int i) {
 }
 
 void CreateModel_dynamic_output_shape_12(Model *model) {
+  OperandType type11(Type::TENSOR_QUANT8_ASYMM, {0, 0}, 4.0f, 100);
   OperandType type2(Type::TENSOR_INT32, {2, 3});
-  OperandType type7(Type::TENSOR_QUANT8_ASYMM, {0, 0}, 4.0f, 100);
   // Phase 1, operands
   auto input02 = model->addOperand(&type2);
-  auto output03 = model->addOperand(&type7);
+  auto output03 = model->addOperand(&type11);
   // Phase 2, operations
   model->addOperation(ANEURALNETWORKS_CAST, {input02}, {output03});
   // Phase 3, inputs and outputs
@@ -762,10 +762,10 @@ inline bool is_ignored_13(int i) {
 
 void CreateModel_dynamic_output_shape_13(Model *model) {
   OperandType type3(Type::TENSOR_QUANT8_ASYMM, {2, 3}, 4.0f, 100);
-  OperandType type4(Type::TENSOR_FLOAT16, {0, 0});
+  OperandType type8(Type::TENSOR_FLOAT16, {0, 0});
   // Phase 1, operands
   auto input03 = model->addOperand(&type3);
-  auto output0 = model->addOperand(&type4);
+  auto output0 = model->addOperand(&type8);
   // Phase 2, operations
   model->addOperation(ANEURALNETWORKS_CAST, {input03}, {output0});
   // Phase 3, inputs and outputs
@@ -824,10 +824,10 @@ inline bool is_ignored_relaxed_7(int i) {
 
 void CreateModel_dynamic_output_shape_14(Model *model) {
   OperandType type3(Type::TENSOR_QUANT8_ASYMM, {2, 3}, 4.0f, 100);
-  OperandType type5(Type::TENSOR_FLOAT32, {0, 0});
+  OperandType type9(Type::TENSOR_FLOAT32, {0, 0});
   // Phase 1, operands
   auto input03 = model->addOperand(&type3);
-  auto output01 = model->addOperand(&type5);
+  auto output01 = model->addOperand(&type9);
   // Phase 2, operations
   model->addOperation(ANEURALNETWORKS_CAST, {input03}, {output01});
   // Phase 3, inputs and outputs
@@ -844,10 +844,10 @@ inline bool is_ignored_dynamic_output_shape_14(int i) {
 
 void CreateModel_dynamic_output_shape_relaxed_7(Model *model) {
   OperandType type3(Type::TENSOR_QUANT8_ASYMM, {2, 3}, 4.0f, 100);
-  OperandType type5(Type::TENSOR_FLOAT32, {0, 0});
+  OperandType type9(Type::TENSOR_FLOAT32, {0, 0});
   // Phase 1, operands
   auto input03 = model->addOperand(&type3);
-  auto output01 = model->addOperand(&type5);
+  auto output01 = model->addOperand(&type9);
   // Phase 2, operations
   model->addOperation(ANEURALNETWORKS_CAST, {input03}, {output01});
   // Phase 3, inputs and outputs
@@ -885,11 +885,11 @@ inline bool is_ignored_15(int i) {
 }
 
 void CreateModel_dynamic_output_shape_15(Model *model) {
+  OperandType type10(Type::TENSOR_INT32, {0, 0});
   OperandType type3(Type::TENSOR_QUANT8_ASYMM, {2, 3}, 4.0f, 100);
-  OperandType type6(Type::TENSOR_INT32, {0, 0});
   // Phase 1, operands
   auto input03 = model->addOperand(&type3);
-  auto output02 = model->addOperand(&type6);
+  auto output02 = model->addOperand(&type10);
   // Phase 2, operations
   model->addOperation(ANEURALNETWORKS_CAST, {input03}, {output02});
   // Phase 3, inputs and outputs
@@ -924,11 +924,11 @@ inline bool is_ignored_16(int i) {
 }
 
 void CreateModel_dynamic_output_shape_16(Model *model) {
+  OperandType type11(Type::TENSOR_QUANT8_ASYMM, {0, 0}, 4.0f, 100);
   OperandType type3(Type::TENSOR_QUANT8_ASYMM, {2, 3}, 4.0f, 100);
-  OperandType type7(Type::TENSOR_QUANT8_ASYMM, {0, 0}, 4.0f, 100);
   // Phase 1, operands
   auto input03 = model->addOperand(&type3);
-  auto output03 = model->addOperand(&type7);
+  auto output03 = model->addOperand(&type11);
   // Phase 2, operations
   model->addOperation(ANEURALNETWORKS_CAST, {input03}, {output03});
   // Phase 3, inputs and outputs
@@ -939,6 +939,170 @@ void CreateModel_dynamic_output_shape_16(Model *model) {
 }
 
 inline bool is_ignored_dynamic_output_shape_16(int i) {
+  static std::set<int> ignore = {};
+  return ignore.find(i) != ignore.end();
+}
+
+void CreateModel_17(Model *model) {
+  OperandType type4(Type::TENSOR_FLOAT16, {2});
+  OperandType type7(Type::TENSOR_QUANT8_ASYMM, {2}, 4.0f, 100);
+  // Phase 1, operands
+  auto input04 = model->addOperand(&type4);
+  auto output04 = model->addOperand(&type7);
+  // Phase 2, operations
+  model->addOperation(ANEURALNETWORKS_CAST, {input04}, {output04});
+  // Phase 3, inputs and outputs
+  model->identifyInputsAndOutputs(
+    {input04},
+    {output04});
+  assert(model->isValid());
+}
+
+inline bool is_ignored_17(int i) {
+  static std::set<int> ignore = {};
+  return ignore.find(i) != ignore.end();
+}
+
+void CreateModel_dynamic_output_shape_17(Model *model) {
+  OperandType type12(Type::TENSOR_QUANT8_ASYMM, {0}, 4.0f, 100);
+  OperandType type4(Type::TENSOR_FLOAT16, {2});
+  // Phase 1, operands
+  auto input04 = model->addOperand(&type4);
+  auto output04 = model->addOperand(&type12);
+  // Phase 2, operations
+  model->addOperation(ANEURALNETWORKS_CAST, {input04}, {output04});
+  // Phase 3, inputs and outputs
+  model->identifyInputsAndOutputs(
+    {input04},
+    {output04});
+  assert(model->isValid());
+}
+
+inline bool is_ignored_dynamic_output_shape_17(int i) {
+  static std::set<int> ignore = {};
+  return ignore.find(i) != ignore.end();
+}
+
+void CreateModel_18(Model *model) {
+  OperandType type5(Type::TENSOR_FLOAT32, {2});
+  OperandType type7(Type::TENSOR_QUANT8_ASYMM, {2}, 4.0f, 100);
+  // Phase 1, operands
+  auto input05 = model->addOperand(&type5);
+  auto output05 = model->addOperand(&type7);
+  // Phase 2, operations
+  model->addOperation(ANEURALNETWORKS_CAST, {input05}, {output05});
+  // Phase 3, inputs and outputs
+  model->identifyInputsAndOutputs(
+    {input05},
+    {output05});
+  assert(model->isValid());
+}
+
+inline bool is_ignored_18(int i) {
+  static std::set<int> ignore = {};
+  return ignore.find(i) != ignore.end();
+}
+
+void CreateModel_relaxed_8(Model *model) {
+  OperandType type5(Type::TENSOR_FLOAT32, {2});
+  OperandType type7(Type::TENSOR_QUANT8_ASYMM, {2}, 4.0f, 100);
+  // Phase 1, operands
+  auto input05 = model->addOperand(&type5);
+  auto output05 = model->addOperand(&type7);
+  // Phase 2, operations
+  model->addOperation(ANEURALNETWORKS_CAST, {input05}, {output05});
+  // Phase 3, inputs and outputs
+  model->identifyInputsAndOutputs(
+    {input05},
+    {output05});
+  // Phase 4: set relaxed execution
+  model->relaxComputationFloat32toFloat16(true);
+  assert(model->isValid());
+}
+
+inline bool is_ignored_relaxed_8(int i) {
+  static std::set<int> ignore = {};
+  return ignore.find(i) != ignore.end();
+}
+
+void CreateModel_dynamic_output_shape_18(Model *model) {
+  OperandType type12(Type::TENSOR_QUANT8_ASYMM, {0}, 4.0f, 100);
+  OperandType type5(Type::TENSOR_FLOAT32, {2});
+  // Phase 1, operands
+  auto input05 = model->addOperand(&type5);
+  auto output05 = model->addOperand(&type12);
+  // Phase 2, operations
+  model->addOperation(ANEURALNETWORKS_CAST, {input05}, {output05});
+  // Phase 3, inputs and outputs
+  model->identifyInputsAndOutputs(
+    {input05},
+    {output05});
+  assert(model->isValid());
+}
+
+inline bool is_ignored_dynamic_output_shape_18(int i) {
+  static std::set<int> ignore = {};
+  return ignore.find(i) != ignore.end();
+}
+
+void CreateModel_dynamic_output_shape_relaxed_8(Model *model) {
+  OperandType type12(Type::TENSOR_QUANT8_ASYMM, {0}, 4.0f, 100);
+  OperandType type5(Type::TENSOR_FLOAT32, {2});
+  // Phase 1, operands
+  auto input05 = model->addOperand(&type5);
+  auto output05 = model->addOperand(&type12);
+  // Phase 2, operations
+  model->addOperation(ANEURALNETWORKS_CAST, {input05}, {output05});
+  // Phase 3, inputs and outputs
+  model->identifyInputsAndOutputs(
+    {input05},
+    {output05});
+  // Phase 4: set relaxed execution
+  model->relaxComputationFloat32toFloat16(true);
+  assert(model->isValid());
+}
+
+inline bool is_ignored_dynamic_output_shape_relaxed_8(int i) {
+  static std::set<int> ignore = {};
+  return ignore.find(i) != ignore.end();
+}
+
+void CreateModel_19(Model *model) {
+  OperandType type6(Type::TENSOR_INT32, {2});
+  OperandType type7(Type::TENSOR_QUANT8_ASYMM, {2}, 4.0f, 100);
+  // Phase 1, operands
+  auto input06 = model->addOperand(&type6);
+  auto output06 = model->addOperand(&type7);
+  // Phase 2, operations
+  model->addOperation(ANEURALNETWORKS_CAST, {input06}, {output06});
+  // Phase 3, inputs and outputs
+  model->identifyInputsAndOutputs(
+    {input06},
+    {output06});
+  assert(model->isValid());
+}
+
+inline bool is_ignored_19(int i) {
+  static std::set<int> ignore = {};
+  return ignore.find(i) != ignore.end();
+}
+
+void CreateModel_dynamic_output_shape_19(Model *model) {
+  OperandType type12(Type::TENSOR_QUANT8_ASYMM, {0}, 4.0f, 100);
+  OperandType type6(Type::TENSOR_INT32, {2});
+  // Phase 1, operands
+  auto input06 = model->addOperand(&type6);
+  auto output06 = model->addOperand(&type12);
+  // Phase 2, operations
+  model->addOperation(ANEURALNETWORKS_CAST, {input06}, {output06});
+  // Phase 3, inputs and outputs
+  model->identifyInputsAndOutputs(
+    {input06},
+    {output06});
+  assert(model->isValid());
+}
+
+inline bool is_ignored_dynamic_output_shape_19(int i) {
   static std::set<int> ignore = {};
   return ignore.find(i) != ignore.end();
 }
