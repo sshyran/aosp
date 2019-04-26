d8b2ce5f98ed158ad7275e5cd48c0507566fec1c
Lev Proleev <levp@google.com>
2019-04-26 11:59:04 +0100

    Add tests covering TENSOR_BOOL8 path for EQUAL and NOT_EQUAL
    Support for TENSOR_BOOL8 in other comarison ops has been dropped in the
    previous CL.
    
    Fix: 129570421
    Test: NeuralNetworksTest_static
    Change-Id: Ia1a624e691d3612e5d4bf1ee6e5f2c0c1c778962

--- nn/runtime/test/generated/tests/not_equal.mod.py.cpp
+++ nn/runtime/test/generated/tests/not_equal.mod.py.cpp
@@ -153,3 +153,15 @@ TEST_F(DynamicOutputShapeTest, not_equal_quantized_overflow_first_input_if_requa
             not_equal::get_examples_quantized_overflow_first_input_if_requantized_dynamic_output_shape());
 }
 
+TEST_F(GeneratedTests, not_equal_boolean) {
+    execute(not_equal::CreateModel_7,
+            not_equal::is_ignored_7,
+            not_equal::get_examples_boolean());
+}
+
+TEST_F(DynamicOutputShapeTest, not_equal_boolean_dynamic_output_shape) {
+    execute(not_equal::CreateModel_dynamic_output_shape_7,
+            not_equal::is_ignored_dynamic_output_shape_7,
+            not_equal::get_examples_boolean_dynamic_output_shape());
+}
+
