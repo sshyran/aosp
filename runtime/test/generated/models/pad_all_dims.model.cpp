e436da1e44baaab1636a3856f9ce788c80c79dca
Slava Shklyaev <slavash@google.com>
2019-04-26 15:00:09 +0100

    Add relaxed execution tests for PAD and PAD_V2
    Bug: 130578515
    Test: NeuralNetworksTest_static
    Change-Id: Ib84fd9c16ba15a9f8b941b1ebaaf4307ddbc380c
    Merged-In: Ib84fd9c16ba15a9f8b941b1ebaaf4307ddbc380c
    (cherry picked from commit aad00ff59ae10656c8c1190727ce08f992162f89)

--- runtime/test/generated/models/pad_all_dims.model.cpp
+++ runtime/test/generated/models/pad_all_dims.model.cpp
@@ -48,6 +48,32 @@ inline bool is_ignored_float16(int i) {
   return ignore.find(i) != ignore.end();
 }
 
+void CreateModel_relaxed(Model *model) {
+  OperandType type0(Type::TENSOR_FLOAT32, {1, 1, 2, 3});
+  OperandType type1(Type::TENSOR_INT32, {4, 2});
+  OperandType type2(Type::TENSOR_FLOAT32, {4, 8, 8, 6});
+  // Phase 1, operands
+  auto input0 = model->addOperand(&type0);
+  auto paddings = model->addOperand(&type1);
+  auto output0 = model->addOperand(&type2);
+  // Phase 2, operations
+  static int32_t paddings_init[] = {1, 2, 3, 4, 3, 3, 2, 1};
+  model->setOperandValue(paddings, paddings_init, sizeof(int32_t) * 8);
+  model->addOperation(ANEURALNETWORKS_PAD, {input0, paddings}, {output0});
+  // Phase 3, inputs and outputs
+  model->identifyInputsAndOutputs(
+    {input0},
+    {output0});
+  // Phase 4: set relaxed execution
+  model->relaxComputationFloat32toFloat16(true);
+  assert(model->isValid());
+}
+
+inline bool is_ignored_relaxed(int i) {
+  static std::set<int> ignore = {};
+  return ignore.find(i) != ignore.end();
+}
+
 void CreateModel_quant8(Model *model) {
   OperandType type1(Type::TENSOR_INT32, {4, 2});
   OperandType type5(Type::TENSOR_QUANT8_ASYMM, {1, 1, 2, 3}, 2.3f, 9);
@@ -116,6 +142,32 @@ void CreateModel_dynamic_output_shape_float16(Model *model) {
 }
 
 inline bool is_ignored_dynamic_output_shape_float16(int i) {
+  static std::set<int> ignore = {};
+  return ignore.find(i) != ignore.end();
+}
+
+void CreateModel_dynamic_output_shape_relaxed(Model *model) {
+  OperandType type0(Type::TENSOR_FLOAT32, {1, 1, 2, 3});
+  OperandType type1(Type::TENSOR_INT32, {4, 2});
+  OperandType type7(Type::TENSOR_FLOAT32, {0, 0, 0, 0});
+  // Phase 1, operands
+  auto input0 = model->addOperand(&type0);
+  auto paddings = model->addOperand(&type1);
+  auto output0 = model->addOperand(&type7);
+  // Phase 2, operations
+  static int32_t paddings_init[] = {1, 2, 3, 4, 3, 3, 2, 1};
+  model->setOperandValue(paddings, paddings_init, sizeof(int32_t) * 8);
+  model->addOperation(ANEURALNETWORKS_PAD, {input0, paddings}, {output0});
+  // Phase 3, inputs and outputs
+  model->identifyInputsAndOutputs(
+    {input0},
+    {output0});
+  // Phase 4: set relaxed execution
+  model->relaxComputationFloat32toFloat16(true);
+  assert(model->isValid());
+}
+
+inline bool is_ignored_dynamic_output_shape_relaxed(int i) {
   static std::set<int> ignore = {};
   return ignore.find(i) != ignore.end();
 }
