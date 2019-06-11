edba8eb4afa7bf24bb05075f5bbd4700cb87535e
Xusong Wang <xusongw@google.com>
2019-06-11 14:42:56 -0700

    Fix PRELU underflow/overflow issue.
    The quant8 results should be clamped by 0 and 255 before static casting
    to uint8_t.
    
    Bug: 134800353
    Test: NeuralNetworksTest_static_fuzzing
    Change-Id: I1bc8f386782c67158edd3da2f663fee5746dda48
    Merged-In: I1bc8f386782c67158edd3da2f663fee5746dda48
    (cherry picked from commit 4e421c61942b2c2f5bbdf756bba6e1c6e97bbcc8)

--- common/operations/PRelu.cpp
+++ common/operations/PRelu.cpp
@@ -86,6 +86,7 @@ bool evalQuant8(const uint8_t* aData, const Shape& aShape, const uint8_t* bData,
                                  tflite::MultiplyByQuantizedMultiplier(
                                          input * alpha, output_multiplier_neg, output_shift_neg);
                 }
+                output_val = std::max(0, std::min(255, output_val));
                 return static_cast<uint8_t>(output_val);
             },
             aData, aShape, bData, bShape, outputData, outputShape);
