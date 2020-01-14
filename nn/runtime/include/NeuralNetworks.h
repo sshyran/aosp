1ef2107a42a0cd15395d08daa33b45a9b235abc4
Lev Proleev <levp@google.com>
2020-01-14 17:35:36 +0000

    Add a state output for BIDIRECTIONAL_SEQUENCE_RNN
    Bug: 138443991
    Test: NNTest_static and VtsHalNeuralnetworksV1_3TargetTest
    Change-Id: Iabda8320facc253c273ea6edb9d0b24b3715c4f9
    Merged-In: Iabda8320facc253c273ea6edb9d0b24b3715c4f9
    (cherry picked from commit 7aee1ca40e9e0c18ad7a31c5ac3f5f056da44a29)

--- nn/runtime/include/NeuralNetworks.h
+++ nn/runtime/include/NeuralNetworks.h
@@ -2920,6 +2920,18 @@ typedef enum {
      *      (timeMajor). If it is set to true, then the shape is set to
      *      [maxTime, batchSize, bwNumUnits], otherwise the shape is set to
      *      [batchSize, maxTime, bwNumUnits].
+     * * 2: The forward hidden state output.
+     *      A 2-D tensor of shape [batchSize, fwNumUnits] containing a hidden
+     *      state from the last time step in the sequence. This output is
+     *      optional and can be omitted. If this output is present then output
+     *      3 must be present as well.
+     *      Available since API level 30.
+     * * 3: The backward hidden state output.
+     *      A 2-D tensor of shape [batchSize, bwNumUnits] containing a hidden
+     *      state from the last time step in the sequence. This output is
+     *      optional and can be omitted. If this output is present then output
+     *      2 must be present as well.
+     *      Available since API level 30.
      *
      * Available since API level 29.
      *
