ed2d2e37c569881a18cd2b3a1dc3dd3eee13849d
Slava Shklyaev <slavash@google.com>
2020-01-30 17:06:25 +0000

    Add control flow support to test harness
    This change introduces a new TestModel structure that mirrors the HAL
    changes in I7a75175f.
    
    Bug: 148601177
    Bug: 136735929
    Test: NNT_static
    Change-Id: I98a3edd11f9ef74abbe148a2dcf13e8a5f74bc06
    Merged-In: I98a3edd11f9ef74abbe148a2dcf13e8a5f74bc06
    (cherry picked from commit dcbbad9ecd630fcc0155d124b77afaa323fb3885)

--- runtime/test/TestNeuralNetworksWrapper.h
+++ runtime/test/TestNeuralNetworksWrapper.h
@@ -116,9 +116,13 @@ class Model {
             mModel = other.mModel;
             mNextOperandId = other.mNextOperandId;
             mValid = other.mValid;
+            mRelaxed = other.mRelaxed;
+            mFinished = other.mFinished;
             other.mModel = nullptr;
             other.mNextOperandId = 0;
             other.mValid = false;
+            other.mRelaxed = false;
+            other.mFinished = false;
         }
         return *this;
     }
@@ -129,6 +133,7 @@ class Model {
             if (result != Result::NO_ERROR) {
                 mValid = false;
             }
+            mFinished = true;
             return result;
         } else {
             return Result::BAD_STATE;
@@ -223,6 +228,7 @@ class Model {
     ANeuralNetworksModel* getHandle() const { return mModel; }
     bool isValid() const { return mValid; }
     bool isRelaxed() const { return mRelaxed; }
+    bool isFinished() const { return mFinished; }
 
    protected:
     ANeuralNetworksModel* mModel = nullptr;
@@ -230,6 +236,7 @@ class Model {
     uint32_t mNextOperandId = 0;
     bool mValid = true;
     bool mRelaxed = false;
+    bool mFinished = false;
 };
 
 class Compilation {
