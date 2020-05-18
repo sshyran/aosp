1021ee62dfcc9ecc05fa255669e86e749f9d7581
Stuart Langley <slangley@chromium.org>
2020-05-18 14:10:45 +1000

    nn: Add initial BUILD.gn file with a simple test.
    This adds a skeleton BUILD file and a includes tests that validates that
    the required header file has been placed into the system build
    directories and are discoverable.
    
    BUG=b:155344260
    TEST=FEATURES="test" emerge-octopus chromeos-base/aosp-frameworks-ml-nn
    
    Cq-Depend: chromium:2206061
    Change-Id: I9a7a7a86e08c0cc0484a40733ac8d0a4f601056b
    Reviewed-on: https://chromium-review.googlesource.com/c/aosp/platform/frameworks/ml/+/2206315
    Reviewed-by: Alan Green <avg@chromium.org>
    Tested-by: Stuart Langley <slangley@chromium.org>
    Commit-Queue: Stuart Langley <slangley@chromium.org>

---
+++ nn/chromeos/tests/common/includes_test.cc
@@ -0,0 +1,16 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <gtest/gtest.h>
+
+#include "GraphDump.h"
+#include "TokenHasher.h"
+
+// This is not really a unit test, but a compile test. It ensures that we have
+// the headers we need from tensorflow, ssl and AOSP base libraries have been
+// installed into the right places.
+// TODO(slangley): Remove this test once we have some more pieces linked in and
+// real tests running.
+TEST(IncludesTest, NotReallyATest) {
+}
