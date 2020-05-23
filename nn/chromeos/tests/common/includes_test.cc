972095f4733768a94c813321fcab71bee3faedd9
Stuart Langley <slangley@chromium.org>
2020-05-23 16:18:39 +1000

    nn: Link in the static library libnnapi-common.a.
    Chrome OS specific implementations of core android functions are being
    made available via libnnapi-common. Setup default configuration options
    so that this libary is linked in to nn packages.
    
    To test it is working, update chromeos test to print a log message,
    which is included via android-base.
    
    BUG=b:155457744
    TEST=FEATURES="test" emerge-octopus chromeos-base/aosp-frameworks-ml-nn
    
    Cq-Depend: chromium:2212073, chromium:2212291
    Change-Id: I6b28848ea520b9212842053c42b3a941e66329bd
    Reviewed-on: https://chromium-review.googlesource.com/c/aosp/platform/frameworks/ml/+/2213513
    Reviewed-by: Alan Green <avg@chromium.org>
    Commit-Queue: Stuart Langley <slangley@chromium.org>
    Tested-by: Stuart Langley <slangley@chromium.org>

--- nn/chromeos/tests/common/includes_test.cc
+++ nn/chromeos/tests/common/includes_test.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <android-base/logging.h>
 #include <gtest/gtest.h>
 
 #include "GraphDump.h"
@@ -13,4 +14,10 @@
 // TODO(slangley): Remove this test once we have some more pieces linked in and
 // real tests running.
 TEST(IncludesTest, NotReallyATest) {
+}
+
+// Tests that android-base has been linked correctly and the methods are
+// available.
+TEST(IncludesTest, LogLinkTest) {
+  LOG(INFO) << "Logged Something, so linking must be working";
 }
