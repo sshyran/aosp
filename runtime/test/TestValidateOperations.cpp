882ee8a208045c4fae14ed3758377bc838bedc5c
Stuart Langley <slangley@google.com>
2020-06-17 21:23:22 +1000

    nn: Use standard integer types.
    In 2 places uint_t is used, which is a non standard type. Replace with
    uint32_t as this matches the type that is being passed to the functions
    by the callers.
    
    Bug: None
    Test: mma
    Change-Id: Ifda599982c45bbfef6fbd4e4f9b35bd0792a975c

--- runtime/test/TestValidateOperations.cpp
+++ runtime/test/TestValidateOperations.cpp
@@ -208,7 +208,8 @@ class TensorRankConstraint {
 
    private:
     std::vector<OperandTypeWithExtraParams> ModifyForRank(
-            const std::vector<OperandTypeWithExtraParams>& operandsTypeWithParams, uint_t newRank) {
+            const std::vector<OperandTypeWithExtraParams>& operandsTypeWithParams,
+            uint32_t newRank) {
         std::vector<OperandTypeWithExtraParams> result;
         std::transform(operandsTypeWithParams.cbegin(), operandsTypeWithParams.cend(),
                        std::back_inserter(result),
@@ -219,7 +220,7 @@ class TensorRankConstraint {
     }
 
     OperandTypeWithExtraParams ModifyForRank(
-            const OperandTypeWithExtraParams& operandTypeWithParams, uint_t newRank) {
+            const OperandTypeWithExtraParams& operandTypeWithParams, uint32_t newRank) {
         if (operandTypeWithParams.operandType.dimensionCount == newRank) {
             return operandTypeWithParams;
         }
