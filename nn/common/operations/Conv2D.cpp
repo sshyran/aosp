25e46626738b6d0668a445b6003d8d7d064d11a8
Jim Pollock <jmpollock@google.com>
2020-06-23 19:57:25 +0100

    nn: more input checking for optimized_ops::Conv
    cl/2259941 missed the invocation of Conv for float types. This
    refactors the check and applies it to both calling sites.
    
    Fixes several tests from the generated test suite.
    
    BUG=b:159414785
    TEST=FEATURES=test emerge-betty chromeos-base/aosp-frameworks-ml-nn
    
    Change-Id: I554511cb3eb43fb168fae4b4fd79d1898e38f5fa
    Reviewed-on: https://chromium-review.googlesource.com/c/aosp/platform/frameworks/ml/+/2260600
    Tested-by: Jim Pollock <jmpollock@chromium.org>
    Auto-Submit: Jim Pollock <jmpollock@chromium.org>
    Reviewed-by: Stuart Langley <slangley@chromium.org>
    Commit-Queue: Jim Pollock <jmpollock@chromium.org>

--- nn/common/operations/Conv2D.cpp
+++ nn/common/operations/Conv2D.cpp
@@ -174,6 +174,24 @@ struct Conv2dParam {
         im2colGuard.reset(im2colData);                                            \
     }
 
+bool needim2colData(const Shape& filterShape,
+                    int32_t stride_width, int32_t stride_height,
+                    int32_t dilation_width_factor, int32_t dilation_height_factor) {
+    // Within tflite::optimized_ops::Conv, the following tests are performed,
+    // and in the case (!need_dilated_im2col && !need_im2col), then the
+    // method doesn't expect to receive outputData. In debug mode this is
+    // asserted and fails tests, so we need to perform this check as the caller
+    // also. See:
+    // tensorflow/lite/kernels/internal/optimized/legacy_optimized_ops.h:2655
+    const int filter_width = getSizeOfDimension(filterShape, 2);
+    const int filter_height = getSizeOfDimension(filterShape, 1);
+    const bool need_dilated_im2col =
+        dilation_width_factor != 1 || dilation_height_factor != 1;
+    const bool need_im2col = stride_width != 1 || stride_height != 1 ||
+                            filter_width != 1 || filter_height != 1;
+    return need_dilated_im2col || need_im2col;
+}
+
 bool convNhwc(const float* inputData, const Shape& inputShape, const float* filterData,
               const Shape& filterShape, const float* biasData, const Shape& biasShape,
               int32_t padding_left, int32_t padding_right, int32_t padding_top,
@@ -190,12 +208,20 @@ bool convNhwc(const float* inputData, const Shape& inputShape, const float* filt
     // Prevent concurrent executions that may access the scratch buffer.
     std::unique_lock<std::mutex> lock(executionMutex);
     NNTRACE_COMP_SWITCH("optimized_ops::Conv");
+
+    const bool need_im2colData = needim2colData(filterShape,
+                                                stride_width, stride_height,
+                                                dilation_width_factor,
+                                                dilation_height_factor);
+
     tflite::optimized_ops::Conv(inputData, convertShapeToDims(inputShape), filterData,
                                 convertShapeToDims(filterShape), biasData,
                                 convertShapeToDims(biasShape), stride_width, stride_height,
                                 dilation_width_factor, dilation_height_factor, paddingWidth,
                                 paddingHeight, output_activation_min, output_activation_max,
-                                outputData, convertShapeToDims(outputShape), im2colData, im2colDim);
+                                outputData, convertShapeToDims(outputShape),
+                                need_im2colData ? im2colData : nullptr,
+                                im2colDim);
     return true;
 }
 
@@ -237,19 +263,10 @@ bool convNhwc(const uint8_t* inputData, const Shape& inputShape, const uint8_t* 
 
     NNTRACE_COMP_SWITCH("optimized_ops::Conv");
 
-    // Within tflite::optimized_ops::Conv, the following tests are performed,
-    // and in the case (!need_dilated_im2col && !need_im2col), then the
-    // method doesn't expect to receive outputData. In debug mode this is
-    // asserted and fails tests, so we need to perform this check as the caller
-    // also. See:
-    // tensorflow/lite/kernels/internal/optimized/legacy_optimized_ops.h:2655
-    const int filter_width = getSizeOfDimension(filterShape, 2);
-    const int filter_height = getSizeOfDimension(filterShape, 1);
-    const bool need_dilated_im2col =
-        dilation_width_factor != 1 || dilation_height_factor != 1;
-    const bool need_im2col = stride_width != 1 || stride_height != 1 ||
-                            filter_width != 1 || filter_height != 1;
-    const bool need_im2colData = need_dilated_im2col || need_im2col;
+    const bool need_im2colData = needim2colData(filterShape,
+                                                stride_width, stride_height,
+                                                dilation_width_factor,
+                                                dilation_height_factor);
 
     tflite::optimized_ops::Conv(
             inputData, convertShapeToDims(inputShape), inputOffset, filterData,
