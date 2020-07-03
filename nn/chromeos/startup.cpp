86e55ddeb971902efb61c98fd632b2cf085d80e4
Stuart Langley <slangley@google.com>
2020-07-03 15:40:17 +1000

    nn: Initialize logging when the library is loaded.
    Add a library initialization routine that will correctly initialize the
    Android base logging functions.
    
    BUG=b:160283882
    TEST=FEATURES=test emerge-betty chromeos-base/aosp-frameworks-ml-nn
    TEST=Deploy to device, set environment to bad string and get error.
    
    Change-Id: I0d8e4db15b6d6f7c1f6e27c35520ccff11dadc64
    Reviewed-on: https://chromium-review.googlesource.com/c/aosp/platform/frameworks/ml/+/2279348
    Reviewed-by: Jim Pollock <jmpollock@chromium.org>
    Commit-Queue: Stuart Langley <slangley@chromium.org>
    Tested-by: Stuart Langley <slangley@chromium.org>

---
+++ nn/chromeos/startup.cpp
@@ -0,0 +1,13 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <android-base/logging.h>
+
+// Called when the shared library is first loaded.
+__attribute__((constructor))
+static void library_init() {
+  // Initilialized the Android logging systen, which will read log settings from
+  // the environment variable ANDROID_LOG_TAGS.
+  android::base::InitLogging(nullptr);
+}
