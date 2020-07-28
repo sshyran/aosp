5c3fa1c068c4a37045d108ff8c47307162fff838
Jim Pollock <jmpollock@google.com>
2020-07-28 17:11:12 +0100

    nn: reduce default log verbosity for tests
    Currently a large amount of ERROR severity log messages are printed, but
    are expected due to the nature of the tests. package_test doesn't
    facilitate propagate env variables (ANDROID_LOG_TAGS), so we set it in
    the testrunner as a workaround.
    
    BUG=b:162318479
    TEST=FEATURES=test emerge-betty aosp-frameworks-ml-nn
    
    Change-Id: I8b7e6ffed0ad32d417b3fe3faf9539732a886a43
    Reviewed-on: https://chromium-review.googlesource.com/c/aosp/platform/frameworks/ml/+/2323636
    Tested-by: Jim Pollock <jmpollock@chromium.org>
    Auto-Submit: Jim Pollock <jmpollock@chromium.org>
    Commit-Queue: Stuart Langley <slangley@chromium.org>
    Reviewed-by: Stuart Langley <slangley@chromium.org>

--- nn/chromeos/testrunner.cpp
+++ nn/chromeos/testrunner.cpp
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 #include <memory>
+#include <stdlib.h>
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
@@ -10,6 +11,14 @@
 #include "android-base/logging.h"
 
 int main(int argc, char** argv) {
+  // Our testrunners are invoked with platform_test, which doesn't
+  // pass through env variables like 'ANDROID_LOGS_TAGS'. This is
+  // our workaround to handle the extreme error log verbosity of
+  // the test suites.
+#ifndef WITH_TEST_ERROR_LOGS
+  setenv("ANDROID_LOG_TAGS", "*:f", 0);
+#endif
+
   testing::InitGoogleTest(&argc, argv);
   android::base::InitLogging(argv, android::base::StderrLogger);
   testing::GTEST_FLAG(throw_on_failure) = true;
